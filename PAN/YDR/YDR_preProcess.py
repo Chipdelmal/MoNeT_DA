886143e79ce2385d02955cf68fd1dd1cab8cad19
chipdelmal <chipdelmal@gmail.com>
2020-12-01 15:07:19 -0600

    YSD Looping through experiments

--- PAN/YDR/YDR_preProcess.py
+++ PAN/YDR/YDR_preProcess.py
@@ -8,42 +8,43 @@ import MoNeT_MGDrivE as monet
 from joblib import Parallel, delayed
 
 
-(USR, SET, DRV, AOI, EXP) = ('lap', 'homing', 'XSD', 'HLT', '000')
+(USR, SET, DRV, AOI) = ('lap', 'homing', 'XSD', 'HLT')
 # (USR, EXP, DRV, AOI) = (sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5])
 (FMT, OVW, MF, JOB) = ('bz2', True, (False, True), 8)
 (SUM, AGG, SPA, REP, SRP) = (True, False, False, False, True)
-###############################################################################
-# Setting up paths and style
-###############################################################################
-(drive, land) = (
-    drv.driveSelector(DRV, AOI, popSize=11000),
-    lnd.landSelector('SPA')
-)
-gene = drive.get('gDict')
-fldr = drive.get('folder')
-(PT_ROT, PT_IMG, PT_DTA, PT_PRE, PT_OUT, PT_MTR) = aux.selectPath(
-    USR, SET, fldr, EXP
-)
-drive
-# Time and head ---------------------------------------------------------------
-tS = datetime.now()
-aux.printExperimentHead(PT_ROT, PT_IMG, PT_PRE, tS, 'Preprocess '+AOI)
-###############################################################################
-# Load folders
-###############################################################################
-(expDirsMean, expDirsTrac) = aux.getExpPaths(PT_DTA)
-(expNum, nodeDigits) = (len(expDirsMean), len(str(len(land)))+1)
-outNames = aux.splitExpNames(PT_OUT)
-outExpNames = set(outNames)
-###############################################################################
-# Analyze data
-###############################################################################
-Parallel(n_jobs=JOB)(
-    delayed(monet.preProcess)(
-            exIx, expNum, expDirsMean, expDirsTrac, gene,
-            analysisOI=AOI, prePath=PT_PRE, nodesAggLst=land,
-            outExpNames=outExpNames, fNameFmt='{}/{}-{}_', OVW=OVW,
-            MF=MF, cmpr=FMT, nodeDigits=nodeDigits,
-            SUM=SUM, AGG=AGG, SPA=SPA, REP=REP, SRP=SRP
-    ) for exIx in range(0, expNum)
-)
+EXPS = ('000', '002', '004', '006', '008')
+for EXP in EXPS:
+    ###########################################################################
+    # Setting up paths and style
+    ###########################################################################
+    (drive, land) = (
+        drv.driveSelector(DRV, AOI, popSize=11000),
+        lnd.landSelector('SPA')
+    )
+    gene = drive.get('gDict')
+    fldr = drive.get('folder')
+    (PT_ROT, PT_IMG, PT_DTA, PT_PRE, PT_OUT, PT_MTR) = aux.selectPath(
+        USR, SET, fldr, EXP
+    )
+    # Time and head -----------------------------------------------------------
+    tS = datetime.now()
+    aux.printExperimentHead(PT_ROT, PT_IMG, PT_PRE, tS, 'Preprocess ' + AOI)
+    ###########################################################################
+    # Load folders
+    ###########################################################################
+    (expDirsMean, expDirsTrac) = aux.getExpPaths(PT_DTA)
+    (expNum, nodeDigits) = (len(expDirsMean), len(str(len(land)))+1)
+    outNames = aux.splitExpNames(PT_OUT)
+    outExpNames = set(outNames)
+    ###########################################################################
+    # Analyze data
+    ###########################################################################
+    Parallel(n_jobs=JOB)(
+        delayed(monet.preProcess)(
+                exIx, expNum, expDirsMean, expDirsTrac, gene,
+                analysisOI=AOI, prePath=PT_PRE, nodesAggLst=land,
+                outExpNames=outExpNames, fNameFmt='{}/{}-{}_', OVW=OVW,
+                MF=MF, cmpr=FMT, nodeDigits=nodeDigits,
+                SUM=SUM, AGG=AGG, SPA=SPA, REP=REP, SRP=SRP
+        ) for exIx in range(0, expNum)
+    )
