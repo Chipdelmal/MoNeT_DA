55dd9925850fb4ca6cd8b4592725aaf42568e232
chipdelmal <chipdelmal@gmail.com>
2020-12-02 14:03:57 -0800

    YDR Update on preProcess bash

--- PAN/YDR/YDR_preProcess.sh
+++ PAN/YDR/YDR_preProcess.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
 # chmod +x YDR_preProcess.sh
-python YDR_preProcess.py $1 $2 $3 ECO
-python YDR_preProcess.py $1 $2 $3 HLT
-python YDR_preProcess.py $1 $2 $3 WLD
-python YDR_preProcess.py $1 $2 $3 TRS
+if [ "$2" = "ASD" ] || [ "$2" = "XSD" ] || [ "$2" = "YSD" ]; then
+	aType="homing"
+fi
+if [ "$2" = "AXS" ] || [ "$2" = "YXS" ]; then
+	aType="shredder"
+fi
+python YDR_preProcess.py $1 $aType $2 ECO
+python YDR_preProcess.py $1 $aType $2 HLT
+python YDR_preProcess.py $1 $aType $2 WLD
+python YDR_preProcess.py $1 $aType $2 TRS
