0115435f9eb113546c367ec197957487db4442a7
chipdelmal <chipdelmal@gmail.com>
2020-12-02 16:21:42 -0600

    YDR Uploading preTraces bash script

---
+++ PAN/YDR/YDR_preTraces.sh
@@ -0,0 +1,8 @@
+
+
+# chmod +x YDR_preTraces.sh
+expSet=$(./YDR_selector.sh $2)
+python YDR_preProcess.py $1 $expSet $2 ECO
+python YDR_preProcess.py $1 $expSet $2 HLT
+python YDR_preProcess.py $1 $expSet $2 WLD
+python YDR_preProcess.py $1 $expSet $2 TRS
