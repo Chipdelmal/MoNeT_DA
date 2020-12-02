f5ab88949c8a7b4daa4319b281980734e95c103b
chipdelmal <chipdelmal@gmail.com>
2020-12-02 14:56:48 -0800

    YDR server bash updates

--- PAN/YDR/YDR_preProcess.sh
+++ PAN/YDR/YDR_preProcess.sh
@@ -2,7 +2,7 @@
 
 # chmod +x YDR_preProcess.sh
 expSet=$(./YDR_selector.sh $2)
-python YDR_preProcess.py $1 $aType $2 ECO
-python YDR_preProcess.py $1 $aType $2 HLT
-python YDR_preProcess.py $1 $aType $2 WLD
-python YDR_preProcess.py $1 $aType $2 TRS
+python YDR_preProcess.py $1 $expSet $2 ECO
+python YDR_preProcess.py $1 $expSet $2 HLT
+python YDR_preProcess.py $1 $expSet $2 WLD
+python YDR_preProcess.py $1 $expSet $2 TRS
--- PAN/YDR/YDR_preTraces.sh
+++ PAN/YDR/YDR_preTraces.sh
@@ -2,7 +2,7 @@
 
 # chmod +x YDR_preTraces.sh
 expSet=$(./YDR_selector.sh $2)
-python YDR_preProcess.py $1 $expSet $2 ECO
-python YDR_preProcess.py $1 $expSet $2 HLT
-python YDR_preProcess.py $1 $expSet $2 WLD
-python YDR_preProcess.py $1 $expSet $2 TRS
+python YDR_preTraces.py $1 $expSet $2 ECO
+python YDR_preTraces.py $1 $expSet $2 HLT
+python YDR_preTraces.py $1 $expSet $2 WLD
+python YDR_preTraces.py $1 $expSet $2 TRS
