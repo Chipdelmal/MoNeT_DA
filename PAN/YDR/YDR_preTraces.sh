b84493409b09d50999fcd12afc30890de26d3809
chipdelmal <chipdelmal@gmail.com>
2020-12-03 08:00:59 -0800

    YDR Bash preprocess now calls pretraces

--- PAN/YDR/YDR_preTraces.sh
+++ PAN/YDR/YDR_preTraces.sh
@@ -1,8 +1,8 @@
-
+#!/bin/bash
 
 # chmod +x YDR_preTraces.sh
 expSet=$(./YDR_selector.sh $2)
 python YDR_preTraces.py $1 $expSet $2 ECO
 python YDR_preTraces.py $1 $expSet $2 HLT
-python YDR_preTraces.py $1 $expSet $2 WLD
 python YDR_preTraces.py $1 $expSet $2 TRS
+python YDR_preTraces.py $1 $expSet $2 WLD
