03665c9a4cddf5a3279b97cea564fdd3637006a1
chipdelmal <chipdelmal@gmail.com>
2020-12-08 12:26:58 -0600

    YDR Code-cleanliness updates

--- PAN/YDR/YDR_gene.py
+++ PAN/YDR/YDR_gene.py
@@ -8,20 +8,6 @@ import MoNeT_MGDrivE as monet
 
 
 ###############################################################################
-# Colors
-#   Ecology, Health, Trash, Wild
-###############################################################################
-COLEN = ['#2614ed', '#FF006E', '#45d40c', '#8338EC', '#1888e3', '#BC1097', '#FFE93E', '#3b479d']
-COLHN = ['#FF006E', '#8338EC', '#0C4887']
-COLTN = ['#00a2fe', '#8338EC', '#0C4887']
-COLWN = ['#0eeb10', '#8338EC', '#0C4887']
-# Auto-generate colorsets with required alphas --------------------------------
-(COLEN, COLHN, COLTN, COLWN) = [monet.addHexOpacity(i, alpha='1A') for i in (COLEN, COLHN, COLTN, COLWN)]
-(COLEO, COLHO, COLTO, COLWO) = [monet.replaceHexOpacity(i, alpha='FF') for i in (COLEN, COLHN, COLTN, COLWN)]
-(COLEM, COLHM, COLTM, COLWM) = [monet.generateAlphaColorMapFromColorArray(COLWO) for i in (COLEO, COLHO, COLTO, COLWO)]
-
-
-###############################################################################
 # Drive
 ###############################################################################
 def driveSelector(DRIVE, TYPE, popSize=11000):
@@ -43,13 +29,13 @@ def driveSelector(DRIVE, TYPE, popSize=11000):
         (aggD, yRange, folder) = YXS.driveParameters(TYPE, popSize)
     ###########################################################################
     if TYPE == 'ECO':
-        colors = COLEN
+        colors = monet.COLEN
     elif TYPE == 'HLT':
-        colors = COLHN
+        colors = monet.COLHN
     elif TYPE == 'TRS':
-        colors = COLTN
+        colors = monet.COLTN
     elif TYPE == 'WLD':
-        colors = COLWN
+        colors = monet.COLWN
     ###########################################################################
     if TYPE != 'ECO':
         yRange = yRange / 2
