cde94e7afbcde80b2ef67a78958e8da1a70c5a98
chipdelmal <chipdelmal@gmail.com>
2020-12-08 12:46:57 -0600

    STP Working on getting v2 data running

--- STP/SvR/STP_functions.py
+++ STP/SvR/STP_functions.py
@@ -32,25 +32,7 @@ def getXpId(pFile, idIx):
     return xpId
 
 
-def printExperimentHead(PATH_ROOT, PATH_IMG, PATH_DATA, time, title):
-    print(monet.PAD)
-    (cred, cwht, cend) = (monet.CRED, monet.CWHT, monet.CEND)
-    print(cwht+'UCI '+title+' ['+str(time)+']'+cend)
-    print(monet.PAD)
-    print('{}* Root: {}{}'.format(cred, PATH_ROOT, cend))
-    print('{}* Imgs: {}{}'.format(cred, PATH_IMG, cend))
-    print('{}* Data: {}{}'.format(cred, PATH_DATA, cend))
-    print(monet.PAD)
 
-
-def getExpPaths(PATH_DATA):
-    (expDirsMean, expDirsTrac) = (
-            monet.listDirectoriesWithPathWithinAPath(PATH_DATA + 'ANALYZED/'),
-            monet.listDirectoriesWithPathWithinAPath(PATH_DATA + 'GARBAGE/')
-        )
-    expDirsMean.sort()
-    expDirsTrac.sort()
-    return (expDirsMean, expDirsTrac)
 
 
 def splitExpNames(PATH_OUT, ext='lzma'):
@@ -143,3 +125,4 @@ def quickSaveFigPad(filename, fig, dpi=750, transparent=True):
          orientation='portrait', papertype=None, format='png',
          transparent=transparent, pad_inches=1
      )
+
