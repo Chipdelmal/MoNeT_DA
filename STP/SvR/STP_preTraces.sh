284dbc290318dfe8c74019e940e797676345588a
chipdelmal <chipdelmal@gmail.com>
2020-12-15 14:10:59 -0800

    STP Server running SPA preprocess

--- STP/SvR/STP_preTraces.sh
+++ STP/SvR/STP_preTraces.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-python STP_preTraces.py $1 ECO $2 $3
-python STP_preTraces.py $1 HLT $2 $3
-python STP_preTraces.py $1 TRS $2 $3
-python STP_preTraces.py $1 WLD $2 $3
+python STP_preTraces.py $1 ECO $2 $3 $4
+python STP_preTraces.py $1 HLT $2 $3 $4
+python STP_preTraces.py $1 TRS $2 $3 $4
+python STP_preTraces.py $1 WLD $2 $3 $4
 
 python STP_preGrids.py $1 $2 $3
