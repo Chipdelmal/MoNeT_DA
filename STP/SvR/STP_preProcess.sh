c91aa3c79fee88efaf1f1bbe9d69c1187adb816d
chipdelmal <chipdelmal@gmail.com>
2021-01-04 13:37:35 -0600

    STP Fixing bug in preProcess.sh

--- STP/SvR/STP_preProcess.sh
+++ STP/SvR/STP_preProcess.sh
@@ -11,6 +11,6 @@ python STP_preProcess.py $1 HLT $2 $3 $4
 python STP_preProcess.py $1 TRS $2 $3 $4
 python STP_preProcess.py $1 WLD $2 $3 $4
 
-if [ "$4" = "True" ]; then
+if [ "$5" = "True" ]; then
     ./STP_preTraces.sh "$@"
 fi
