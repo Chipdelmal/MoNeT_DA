e05524f9ad36f3487d18c4723ba4684a9617dc06
chipdelmal <chipdelmal@gmail.com>
2021-01-08 07:41:03 -0800

    STP Server updates

--- STP/SvR/STP_preProcess.sh
+++ STP/SvR/STP_preProcess.sh
@@ -6,10 +6,10 @@
 # argv4: MGDrivE Version
 
 # ./STP_preProcess.sh srv 265 SPA v1
-python STP_preProcess.py $1 ECO $2 $3 $4
 python STP_preProcess.py $1 HLT $2 $3 $4
 python STP_preProcess.py $1 TRS $2 $3 $4
 python STP_preProcess.py $1 WLD $2 $3 $4
+python STP_preProcess.py $1 ECO $2 $3 $4
 
 if [ "$5" = "True" ]; then
     ./STP_preTraces.sh "$@"
