9c2a16d2f7a84f6c8d4506a88a733c0abbf9933b
chipdelmal <chipdelmal@gmail.com>
2021-01-08 09:59:29 -0600

    STP Adding error clause for no experiment files

--- STP/SvR/STP_preVideo.py
+++ STP/SvR/STP_preVideo.py
@@ -21,7 +21,7 @@ import STP_dataAnalysis as da
 import warnings
 warnings.filterwarnings("ignore", category=UserWarning)
 
-EXP = 'E_0020000000_03_0000000100_0100000000_0000015730'
+EXP = 'E_0025000000_03_0000000100_0100000000_0000015730'
 (USR, AOI, REL, LND) = (sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
 (JOB, tmax) = (20, 10 * 365)
 EXP_NAM = '{}-{}'.format(EXP, AOI)
@@ -35,6 +35,9 @@ AG_IDs = lnd.landSelector(LND, REL, PT_ROT)
 UA_sites = pd.read_csv(PT_UAS)
 EXP_FLS = sorted(glob(path.join(PT_PRE, EXP_NAM + '*sum.bz')))
 EXP_VID = path.join(PT_VID, EXP_NAM)
+if len(EXP_FLS) == 0:
+    print("Error: No experiment files were found!")
+    sys.exit()
 monet.makeFolder(PT_VID)
 monet.makeFolder(EXP_VID)
 # #############################################################################
@@ -68,6 +71,6 @@ monet.printExperimentHead(PT_ROT, PT_VID, tS, 'UCIMI PreVideo '+AOI)
 Parallel(n_jobs=JOB)(
     delayed(plo.plotMapFrame)(
         time, UA_sites, BLAT, BLNG, DRV_COL, GC_FRA, lngs, lats, EXP_VID,
-        offset=2.5, amplitude=10, alpha=.35, marker=(6, 0)
+        offset=1, amplitude=10, alpha=.35, marker=(6, 0)
     ) for time in range(0, tmax)
 )
