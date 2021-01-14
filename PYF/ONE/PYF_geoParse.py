2316ea25473a93dfd39df962f8a02cb4644ab4e6
chipdelmal <chipdelmal@gmail.com>
2021-01-14 10:45:44 -0600

    PYF Fixing paths in maps script

--- PYF/ONE/PYF_geoParse.py
+++ PYF/ONE/PYF_geoParse.py
@@ -4,12 +4,17 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import osmnx as ox
+import PYF_aux as aux
+import PYF_plots as plo
 ox.config(log_console=True)
 ox.__version__
 
-PTH = '/home/chipdelmal/Documents/WorkSims/PYF/GEO/'
+
+(PT_ROT, PT_IMG, PT_DTA, PT_PRE, PT_OUT, PT_MTR) = aux.selectPath('dsk', 'Onetahi', 'temp')
+PTH  = '/'.join(PT_ROT.split('/')[:-2]) + '/GEO/'
 point = (-17.0188, -149.5884)
 (placeName, dist) = ('Onetahi', 1000)
+(COLORS, DPI) = (plo.COLORS, 500)
 ###############################################################################
 # Image config
 ###############################################################################
@@ -23,7 +28,7 @@ gdf_proj = ox.project_gdf(gdf)
 fp = f'{PTH}/{placeName}.{extension}'
 fig, ax = ox.plot_footprints(
     gdf_proj, filepath=fp, 
-    dpi=400, save=True, show=False, close=True
+    dpi=DPI, save=False, show=False, close=True
 )
 ###############################################################################
 # Centroids
