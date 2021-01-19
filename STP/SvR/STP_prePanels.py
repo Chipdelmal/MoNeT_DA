4be416c001befbd4ea73ec5bd8f051b0791c1ef2
chipdelmal <chipdelmal@gmail.com>
2021-01-19 15:50:46 -0800

    STP Panels export works but needs improvements

--- STP/SvR/STP_prePanels.py
+++ STP/SvR/STP_prePanels.py
@@ -3,43 +3,56 @@
 # 'inkscape --export-type="png" --export-dpi="300" --export-background="ffffffff" 106_template.svg --export-filename="test.png"'
 
 import re
+from os import path
+import  subprocess
+import STP_aux as aux
+import STP_functions as fun
+import MoNeT_MGDrivE as monet
 
 
 PTRN = 'E_{}_{}_{}_{}_{}-{}'
 
+(USR, LND, REL) = ('dsk', 'SPA', '106')
+(PT_ROT, PT_IMG, PT_DTA, PT_PRE, PT_OUT, PT_MTR) = aux.selectPath(USR, LND, REL)
+monet.makeFolder(path.join(PT_IMG, 'panels'))
 ###############################################################################
 # Template layout
 ###############################################################################
-template = '/home/chipdelmal/Documents/WorkSims/STP/SPA/106/img/106_template.svg'
+template = path.join(PT_IMG, '106_template.svg')
 with open(template, 'r') as file :
-  filedata = file.read()
+    filedata = file.read()
 ###############################################################################
 # Replacements
 ###############################################################################
 # Find original ids -----------------------------------------------------------
 p = re.compile('E_.*png', re.IGNORECASE)
 pats = re.findall(p, filedata)
-img_org = {i[:-8] for i in pats}
-
-(_, i_rer, i_ren, i_rsg, i_fic, i_gsv, i_aoi) = (
+img_org = list({i[:-8] for i in pats})
+# Replacement ids -------------------------------------------------------------
+uids = fun.getExperimentsIDSets(PT_PRE, skip=-1)
+(i_rer, i_ren, i_rsg, i_fic, i_gsv, i_aoi) = (
     '0025000000', '03', '0000000000',
-    '0000000000', ('0000000000', '0100000000'), '0000000000',
-    'HLT'
+    ('0000000000', '0100000000'), 
+    uids[5][1], 'WLD'
 )
+fn = PTRN.format(i_rer, i_ren, i_rsg, 'X', i_gsv, i_aoi)
+print(fn)
 img_new = (
     PTRN.format(i_rer, i_ren, i_rsg, i_fic[0], i_gsv, i_aoi),
     PTRN.format(i_rer, i_ren, i_rsg, i_fic[1], i_gsv, i_aoi)
 )
+# Replace and export SVG ------------------------------------------------------
 fd = filedata.replace(img_org[0], img_new[0])
 fd = fd.replace(img_org[1], img_new[1])
-
-i_fic
-
-pth = "/home/chipdelmal/Documents/WorkSims/STP/SPA/106/img/preTraces/E_0025000000_03_0000000000_0000000000_0000000000-HLT_003.png"
-# Replace string
-imgRef = pth.split('/')
-(imgPth, imgNme) = ('/'.join(imgRef[:-1]), imgRef[-1])
-ptrn.format(i_rer, i_ren, i_rsg, i_fic[0], i_gsv, i_aoi, imgNme[-7:])
-ptrn.format(i_rer, i_ren, i_rsg, i_fic[1], i_gsv, i_aoi, imgNme[-7:])
-
-
+with open(path.join(path.join(PT_IMG, fn+'.svg')), "w") as text_file:
+    text_file.write(fd)
+###############################################################################
+# Export PNG from SVG
+###############################################################################
+cmd = [
+    'inkscape', '--export-type="png"', 
+    '--export-dpi="300"', '--export-background="ffffffff"',
+    path.join(PT_IMG, fn+'.svg'), 
+    '--export-filename='+path.join(PT_IMG, 'panels', fn)
+]
+os.system(' '.join(cmd))
