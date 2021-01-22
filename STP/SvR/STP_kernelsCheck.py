4470a35b125b5e51e349de221ba9a47334960d1b
chipdelmal <chipdelmal@gmail.com>
2021-01-22 15:54:16 -0800

    STP Coms with buffers are working

--- STP/SvR/STP_kernelsCheck.py
+++ STP/SvR/STP_kernelsCheck.py
@@ -3,6 +3,7 @@
 import math
 import shapely
 import numpy as np
+from random import shuffle
 from os import path
 import pandas as pd
 import networkx as nx
@@ -23,6 +24,7 @@ import STP_functions as fun
 
 (PTH, LND) = ('/home/chipdelmal/Documents/WorkSims/STP/SPA/GEO/', 0)
 COLORS = pts.COLORS
+BASE_MAP = True
 ###############################################################################
 # Select land
 ###############################################################################
@@ -48,6 +50,7 @@ elif LND==2:
 ###############################################################################
 psi = np.loadtxt(open(PT_MTR, "rb"), delimiter=",", skiprows=0)
 points = pd.read_csv(PT_PTS)
+longlats = points[['lon', 'lat']]
 ###############################################################################
 # Pre process
 ###############################################################################
@@ -57,7 +60,7 @@ pIn = np.sum(psi.T, axis=0)
 print('ProbIn: {}'.format(pIn))
 print('No access: {}'.format(list(np.where(pIn == 0)[0])))
 psiN = normalize(psi, axis=1, norm='l2')
-(fig, ax) = pts.plotMatrix(psiN)
+# (fig, ax) = pts.plotMatrix(psiN)
 # fun.quickSave(fig, ax, PT_IMG, 'transitions.png', dpi=2000)
 ##############################################################################
 # Transitions Matrix and Base Netowrk
@@ -75,7 +78,7 @@ labelsN = [fun.find_in_list_of_list(comsID, i)[0] for i in range(ptsNum)]
 ##############################################################################
 # Geographic Clustering
 ##############################################################################
-kmeans = AgglomerativeClustering(n_clusters=len(comsID)).fit(points)
+kmeans = AgglomerativeClustering(n_clusters=len(comsID)).fit(longlats)
 labels = list(kmeans.labels_)
 ##############################################################################
 # Plot
@@ -83,6 +86,7 @@ labels = list(kmeans.labels_)
 (width, alpha) = (.5, 5)
 cmap = matplotlib.cm.get_cmap('gist_rainbow', len(set(labels)))
 cmapP = matplotlib.cm.get_cmap('cmr.guppy', len(set(labelsN)))
+shuffle(cmapP.colors)
 (fig, ax) = plt.subplots(1, 1)
 # Map -----------------------------------------------------------------------
 xy = points[['lon', 'lat']]
@@ -105,23 +109,22 @@ mL = Basemap(
     llcrnrlon=minx, urcrnrlon=maxx,
     lat_ts=20, resolution='i', ax=ax
 )
-# mL.drawcoastlines(color=pts.COLORS[4], linewidth=5, zorder=0)
+mL.drawcoastlines(color=pts.COLORS[4], linewidth=5, zorder=-5)
 # Points --------------------------------------------------------------------
 points['marker'] = 'o'
 points['community'] = labelsN
 points['color'] = [cmapP.colors[i] for i in points['community']]
+if not BASE_MAP:
+    cols = list(points['color'])
+    fNameO = 'Coms.png'
+else:
+    cols = '#04011f'
+    fNameO = 'Base.png'
 mL.scatter(
-    list(points['lon']), list(points['lat']), c=list(points['color']), #c='#04011f', # ,
-    marker='o', s=[1 + 1.5 * math.sqrt(i/100) for i in list(points['pop'])], 
+    list(points['lon']), list(points['lat']), c=cols,
+    marker='o', s=[1 + 1.75 * math.sqrt(i/100) for i in list(points['pop'])], 
     alpha=1, lw=.25, zorder=5, latlon=True, edgecolors='w'
 )
-# if PLT_ID:
-#     for i in range(len(points)):
-#         plt.text(
-#             points[i][0] - 0.025, points[i][1] - 0.025, labelsN[i],
-#             horizontalalignment='center', verticalalignment='center',
-#             fontsize=1.25, zorder=6, c='#04011f'
-#         )
 ax.set_aspect(1)
 ax.set_axis_off()
 ax.axes.xaxis.set_ticklabels([])
@@ -129,31 +132,32 @@ ax.axes.yaxis.set_ticklabels([])
 ax.axes.xaxis.set_visible(False)
 ax.axes.yaxis.set_visible(False)
 # Polygons -----------------------------------------------------------------
-r=.01
 X = np.asarray(points[['lon', 'lat']])
-D = fun.disjoint_polygons(X, radius=r, n_angles=100)
-for j in list(set(labels)):
-    matches = [key for key, val in enumerate(labels) if val in set([j])]
-    base = D.geometry[matches[0]]
-    for i in range(len(matches)):
-        base = base.union(D.geometry[matches[i]].buffer(r))
-        mpoly = shapely.ops.transform(mH, base)
-    ax.add_patch(
-        PolygonPatch(
-            mpoly, fc='white', # cmap(j), 
-            ec='none', alpha=1, zorder=-4
+if not BASE_MAP:
+    r=.025
+    D = fun.disjoint_polygons(X, radius=r, n_angles=10)
+    for j in list(set(labels)):
+        matches = [key for key, val in enumerate(labels) if val in set([j])]
+        base = D.geometry[matches[0]]
+        for i in range(len(matches)):
+            base = base.union(D.geometry[matches[i]].buffer(.01*r))
+            mpoly = shapely.ops.transform(mH, base)
+        ax.add_patch(
+            PolygonPatch(
+                mpoly, fc='white', # cmap(j), 
+                ec='none', alpha=1, zorder=-4
+            )
         )
-    )
-    ax.add_patch(
-        PolygonPatch(
-            mpoly, fc="none", ec='#6347ff', lw=1, alpha=.2, zorder=-3
+        ax.add_patch(
+            PolygonPatch(
+                mpoly, fc="none", ec='#6347ff', lw=1.5, alpha=.2, zorder=-3
+            )
         )
-    )
-    ax.add_patch(
-        PolygonPatch(
-            mpoly, fc="none", ec='#ffffff', lw=.25, alpha=1, zorder=-1
+        ax.add_patch(
+            PolygonPatch(
+                mpoly, fc="none", ec='#ffffff', lw=.5, alpha=1, zorder=-1
+            )
         )
-    )
 # Network ------------------------------------------------------------------
-# pts.plotNetworkOnMap(mL, psiN, X, X, c='#04011f', lw=.1)
-fun.quickSave(fig, ax, PT_IMG, 'centrality.png')
+pts.plotNetworkOnMap(mL, psiN, X, X, c='#04011f', lw=.1)
+fun.quickSave(fig, ax, PT_IMG, fNameO)
