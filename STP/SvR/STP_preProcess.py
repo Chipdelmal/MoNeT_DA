5c608b02dbf472753ad2dfae6b690f3a98e81f9e
chipdelmal <chipdelmal@gmail.com>
2021-01-27 10:23:36 -0800

    STP Server sync

--- STP/SvR/STP_preProcess.py
+++ STP/SvR/STP_preProcess.py
@@ -11,8 +11,8 @@ import MoNeT_MGDrivE as monet
 from joblib import Parallel, delayed
 
 
-# (USR, AOI, REL, LND, MGV) = (sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5])
-(USR, AOI, REL, LND, MGV) = ('dsk', 'HLT', '265P', 'SPA', 'v1')
+(USR, AOI, REL, LND, MGV) = (sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5])
+# (USR, AOI, REL, LND, MGV) = ('dsk', 'HLT', '265P', 'SPA', 'v1')
 # (USR, AOI, REL, LND, MGV) = ('srv', 'HLT', 'male', 'EPI', 'v2')
 (DRV, FMT, OVW, JOB) = ('LDR', 'bz2', True, 40)
 (SUM, AGG, SPA, REP, SRP) = (True, False, False, False, True)
