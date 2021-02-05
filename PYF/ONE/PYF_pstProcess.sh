754d59bbb1a67b5d700f8de83863fb8a71577a86
chipdelmal <chipdelmal@gmail.com>
2021-02-05 13:50:00 -0800

    PYF Update pstProcess.sh script

--- PYF/ONE/PYF_pstProcess.sh
+++ PYF/ONE/PYF_pstProcess.sh
@@ -6,6 +6,12 @@
 # 3: LND
 # 4: QNT
 
-python PYF_pstProcess.py $1 $2 HLT $3 $4
-python PYF_pstTraces.py $1 $2 HLT $3 $4
-python PYF_grids.py $1 $2 HLT $3 PST
+# PstFraction --------------------------------
+python PYF_pstFraction.py $1 $2 HLT $3
+# PstProcess ---------------------------------
+python PYF_pstProcess.py $1 $2 HLT $3 50
+python PYF_pstProcess.py $1 $2 HLT $3 75
+python PYF_pstProcess.py $1 $2 HLT $3 90
+# PstPlots ------------------------------------
+# python PYF_pstTraces.py $1 $2 HLT $3 $4
+# python PYF_grids.py $1 $2 HLT $3 PST
