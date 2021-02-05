1d869e98b8b81aa4a849f8b50793a35be55b27d4
chipdelmal <chipdelmal@gmail.com>
2021-02-04 16:50:13 -0800

    PYF preProcess.sh is finished

---
+++ PYF/ONE/PYF_grids.py
@@ -0,0 +1,55 @@
+
+import sys
+from os import path
+from glob import glob
+from datetime import datetime
+import PYF_aux as aux
+import PYF_gene as drv
+import PYF_land as lnd
+import MoNeT_MGDrivE as monet
+from cv2 import imread, imwrite, hconcat, vconcat
+
+
+if monet.isNotebook():
+    (USR, DRV, AOI, LND, STG) = ('dsk', 'PGS', 'HLT', 'SPA', 'PRE')
+else:
+    (USR, DRV, AOI, LND, STG) = (
+        sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5]
+    )
+###############################################################################
+(FMT, SKP, FZ) = ('bz2', False, False)
+AOI = ['HLT']
+# #############################################################################
+# Setup paths and drive
+# #############################################################################
+(PT_ROT, PT_IMG, PT_DTA, PT_PRE, PT_OUT, PT_MTR) = aux.selectPath(USR, LND)
+if STG == 'PRE':
+    (PT_IMG_I, PT_IMG_O) = (
+        path.join(PT_IMG, 'preTraces/'), path.join(PT_IMG, 'preGrids/')
+    )
+else:
+    (PT_IMG_I, PT_IMG_O) = (
+        path.join(PT_IMG, 'pstTraces/'), path.join(PT_IMG, 'pstGrids/')
+    )
+drive = drv.driveSelector(DRV, AOI[0], popSize=20*62)
+land = lnd.landSelector(LND, PT_ROT)
+monet.makeFolder(PT_IMG_O)
+# #############################################################################
+# Get Files
+# #############################################################################
+tS = datetime.now()
+monet.printExperimentHead(PT_IMG_I, PT_IMG_O, tS, 'Grids ')
+# Get files -------------------------------------------------------------------
+NODE_NUM = len(land)
+imgLists = [glob('{}*{}*{}*'.format(PT_IMG_I, i, '*')) for i in AOI]
+imgTuples = list(zip(*[sorted(i) for i in imgLists]))
+imgChunks = list(monet.divideListInChunks(imgTuples, NODE_NUM))[:-1]
+# #############################################################################
+# Iterate through images
+# #############################################################################
+(xpNum, digs) = monet.lenAndDigits(imgChunks)
+for (i, chunk) in enumerate(imgChunks):
+    monet.printProgress(i+1, xpNum, digs)
+    expGrid = hconcat([vconcat([imread(i) for i in j]) for j in chunk])
+    fName = chunk[0][0].split('/')[-1].split('-')[0]
+    imwrite(PT_IMG_O + fName + '.png', expGrid)
--- PYF/ONE/PYF_preTraces.py
+++ PYF/ONE/PYF_preTraces.py
@@ -14,7 +14,7 @@ if monet.isNotebook():
     (USR, DRV, AOI, LND) = ('dsk', 'PGS', 'HLT', 'PAN')
 else:
     (USR, DRV, AOI, LND) = (sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
-FZ = False
+(FZ, tStable) = (False, int(30/2))
 ###############################################################################
 # Setting up paths
 ###############################################################################
@@ -27,9 +27,9 @@ monet.makeFolder(PT_IMG)
 drive = drv.driveSelector(DRV, AOI, popSize=20*62)
 land = lnd.landSelector(LND, PT_ROT)
 gene = drive.get('gDict')
-###########################################################################
+###############################################################################
 # Style 
-###########################################################################
+###############################################################################
 (CLR, YRAN) = (drive.get('colors'), (0, drive.get('yRange')))
 STYLE = {
         "width": .3, "alpha": .5, "dpi": 300, "legend": True,
@@ -39,22 +39,28 @@ STYLE = {
 STYLE['aspect'] = monet.scaleAspect(1, STYLE)
 tS = datetime.now()
 monet.printExperimentHead(PT_PRE, PT_IMG, tS, 'PreTraces ' + AOI)
-###########################################################################
+###############################################################################
 # Load preprocessed files lists
-###########################################################################
+###############################################################################
 tyTag = ('sum', 'srp')
 (fltrPattern, globPattern) = ('dummy', PT_PRE+'*'+AOI+'*'+'{}'+'*')
 if FZ:
     fltrPattern = PT_PRE+'*_00_*'+AOI+'*'+'{}'+'*'
 fLists = monet.getFilteredTupledFiles(fltrPattern, globPattern, tyTag)
-###########################################################################
+###############################################################################
 # Process files
-###########################################################################
+###############################################################################
 (xpNum, digs) = monet.lenAndDigits(fLists)
 for i in range(0, xpNum):
     monet.printProgress(i+1, xpNum, digs)
     (sumDta, repDta) = [pkl.load(file) for file in (fLists[i])]
     name = fLists[i][0].split('/')[-1].split('.')[0][:-4]
+    # Traces ------------------------------------------------------------------
+    balPop = sum(sumDta['population'][tStable])
+    STYLE['yRange'] = (0,  balPop/2+balPop*.5)
+    if AOI == 'ECO':
+        STYLE['yRange'] = (STYLE['yRange'][0], STYLE['yRange'][1]*2)
+    STYLE['aspect'] = monet.scaleAspect(1, STYLE)
     # Export plots --------------------------------------------------------
     monet.exportTracesPlot(repDta, name, STYLE, PT_IMG, wopPrint=False)
     cl = [i[:-2]+'cc' for i in CLR]
