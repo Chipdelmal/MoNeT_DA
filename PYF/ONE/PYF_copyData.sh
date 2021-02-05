9484d5a1793909dba2e1b24d50ab06b9f02717dc
chipdelmal <chipdelmal@gmail.com>
2021-02-05 13:44:36 -0800

    STP server sunc

--- PYF/ONE/PYF_copyData.sh
+++ PYF/ONE/PYF_copyData.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-cp -r /RAID5/marshallShare/pyf/ANALYZED/ /RAID5/marshallShare/pyf/PAN/
-cp -r /RAID5/marshallShare/pyf/TRACE/ /RAID5/marshallShare/pyf/PAN/
+mkdir PAN
+mkdir SPA
 
-cp -r /RAID5/marshallShare/pyf/ANALYZED/ /RAID5/marshallShare/pyf/SPA/
-cp -r /RAID5/marshallShare/pyf/TRACE/ /RAID5/marshallShare/pyf/SPA/
+# cp -r /RAID5/marshallShare/pyf/ANALYZED /RAID5/marshallShare/pyf/PAN/
+cp -r /RAID5/marshallShare/pyf/TRACE /RAID5/marshallShare/pyf/PAN/
 
-rm -r /RAID5/marshallShare/pyf/ANALYZED
-rm -r /RAID5/marshallShare/pyf/TRACE
+# cp /RAID5/marshallShare/pyf/ANALYZED/ /RAID5/marshallShare/pyf/SPA/
+# cp /RAID5/marshallShare/pyf/TRACE/ /RAID5/marshallShare/pyf/SPA/
+
+# rm -r /RAID5/marshallShare/pyf/ANALYZED
+# rm -r /RAID5/marshallShare/pyf/TRACE
