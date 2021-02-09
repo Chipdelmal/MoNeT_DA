99e282ac00af1c811943efb34e8954525858b016
chipdelmal <chipdelmal@gmail.com>
2021-02-09 14:16:45 -0800

    PYF Videos set bash script is up

--- PYF/ONE/PYF_videoSet.sh
+++ PYF/ONE/PYF_videoSet.sh
@@ -1,7 +1,6 @@
+#!/bin/bash
 
-
-
-bash ./PYF_preVideo.sh $1 PGS $2 SPA E_016_024_050_050_050
-bash ./PYF_preVideo.sh $1 PGS $2 SPA E_064_024_050_025_025
-bash ./PYF_preVideo.sh $1 PGS $2 SPA E_064_022_050_050_050
-bash ./PYF_preVideo.sh $1 PGS $2 SPA E_016_024_200_000_000
+bash ./PYF_video.sh $1 $2 E_016_024_050_050_050
+bash ./PYF_video.sh $1 $2 E_064_024_050_025_025
+bash ./PYF_video.sh $1 $2 E_064_022_050_050_050
+bash ./PYF_video.sh $1 $2 E_016_024_200_000_000
