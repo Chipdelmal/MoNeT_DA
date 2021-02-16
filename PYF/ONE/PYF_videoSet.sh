b711b6cf000b91f0d2f142e847bc6f00c58907a9
chipdelmal <chipdelmal@gmail.com>
2021-02-16 08:25:36 -0800

    PYF Video set update

--- PYF/ONE/PYF_videoSet.sh
+++ PYF/ONE/PYF_videoSet.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-bash ./PYF_video.sh $1 $2 E_032_024_200_000_000
-bash ./PYF_video.sh $1 $2 E_032_024_050_050_050
+bash ./PYF_video.sh $1 $2 E_016_024_050_000_000
+bash ./PYF_video.sh $1 $2 E_016_024_100_000_000
+bash ./PYF_video.sh $1 $2 E_016_024_150_000_000
+bash ./PYF_video.sh $1 $2 E_016_024_200_000_000
+
+bash ./PYF_video.sh $1 $2 E_016_024_050_025_025
+bash ./PYF_video.sh $1 $2 E_016_024_100_025_025
+bash ./PYF_video.sh $1 $2 E_016_024_150_025_025
+bash ./PYF_video.sh $1 $2 E_016_024_200_025_025
+
+# bash ./PYF_video.sh $1 $2 E_032_024_050_050_050
 # bash ./PYF_video.sh $1 $2 E_032_024_050_025_025
 # bash ./PYF_video.sh $1 $2 E_032_022_050_050_050
