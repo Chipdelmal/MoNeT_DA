70491d434662c1930140d609a397928f045517f1
chipdelmal <chipdelmal@gmail.com>
2021-02-17 10:34:43 -0800

    v2 Further updates on plots

--- PAN/V2/v2_ecoPlots.py
+++ PAN/V2/v2_ecoPlots.py
@@ -37,6 +37,7 @@ else:
 )
 PT_GEO = (PT_ROT + 'GEO/')
 PT_TRA = (PT_ROT + 'epitraces/E_001/')
+PT_ANL = (PT_ROT + 'analyzed/E_001/')
 (drive, land) = (drv.driveSelector(DRV, AOI, popSize=15000), ([0], ))
 MF = (True, True)
 if AOI == 'HLT':
@@ -62,17 +63,25 @@ epiFiles = sorted(glob(PT_TRA+'/incidence*.csv'))
 epi = [np.loadtxt(i, skiprows=1, delimiter=',', usecols=(1, )) for i in epiFiles]
 epi = [[i / stpPop * 1000 for i in j] for j in epi]
 epiMx = max([max(i) for i in epi])
+# Mosquito Data ---------------------------------------------------------------
+mossy = [i[-1] for i in pkl.load(PT_PRE + 'E_001-HLT_00_sum.bz')['population']]
+infected = np.sum(
+    np.loadtxt(
+        PT_ANL+'FI_Mean_0001.csv', skiprows=1, delimiter=',', 
+        usecols=list(range(1, 30))
+    ), axis=1
+)
 ###############################################################################
 # Epi Plot
 ###############################################################################
 (fig, ax) = plt.subplots(figsize=(10, 5.5), sharex=True)
 STYLE = {
-    "width": .01, "alpha": .5, "dpi": 1500, "legend": True,
+    "width": .01, "alpha": .35, "dpi": 1500, "legend": True,
     "aspect": .25, "xRange": [0, (365*10)],
     "yRange": (0, 1)
 }
 STYLE['aspect'] = monet.scaleAspect(.125, STYLE)
-colors = ('#f20060A9', )
+colors = ('#f20060B9', )
 for i in epi:
     tmp = [j/epiMx for j in i]
     ax.plot(range(0, len(tmp)), tmp, lw=STYLE['width'], color=colors[0])
@@ -137,12 +146,12 @@ plt.close('all')
 temp = temperature / max(temperature)
 (fig, ax) = plt.subplots(figsize=(10, 5.5), sharex=True)
 STYLE = {
-    "width": .1, "alpha": .5, "dpi": 1500, "legend": True,
+    "width": .1, "alpha": .35, "dpi": 1500, "legend": True,
     "aspect": .25, "xRange": [0, (365*10)*24],
     "yRange": (0, 1)
 }
 STYLE['aspect'] = monet.scaleAspect(.125, STYLE)
-colors = ('#f20060FF', '#f20060FF')
+colors = ('#f20060FF', '#f20060DF')
 ax.plot(range(0, len(temp)), temp, lw=STYLE['width'], color=colors[1])
 # for vline in list(range(3*365*24, 3*365*24 + 24*7*8, 24*7)):
 #     ax.axvline(vline, alpha=.75, zorder=10, lw=.1, color='#000000')
@@ -160,6 +169,62 @@ ax.axes.yaxis.set_visible(False)
 ax.set_axis_off()
 fig.savefig(
         "{}/{}.png".format(PT_IMG, 'Temp'),
+        dpi=STYLE['dpi'], facecolor=None, edgecolor='w',
+        orientation='portrait', papertype=None, format='png',
+        transparent=True, bbox_inches='tight', pad_inches=0
+    )
+plt.close('all')
+###############################################################################
+# Mossy Plot
+###############################################################################
+mosquito = mossy / max(mossy)
+(fig, ax) = plt.subplots(figsize=(10, 5.5), sharex=True)
+STYLE = {
+    "width": .35, "alpha": .5, "dpi": 1500, "legend": True,
+    "aspect": .25, "xRange": [0, (365*10)],
+    "yRange": (0, 1)
+}
+STYLE['aspect'] = monet.scaleAspect(.125, STYLE)
+colors = ('#0eeb10FF', )
+ax.plot(range(0, len(mosquito)), mosquito, lw=STYLE['width'], color=colors[0])
+ax.set_xlim(STYLE['xRange'][0], STYLE['xRange'][1])
+ax.set_ylim(STYLE['yRange'][0], STYLE['yRange'][1])
+ax.set_aspect(aspect=STYLE["aspect"])
+ax.axes.xaxis.set_ticklabels([])
+ax.axes.yaxis.set_ticklabels([])
+ax.axes.xaxis.set_visible(False)
+ax.axes.yaxis.set_visible(False)
+ax.set_axis_off()
+fig.savefig(
+        "{}/{}.png".format(PT_IMG, 'Mossy'),
+        dpi=STYLE['dpi'], facecolor=None, edgecolor='w',
+        orientation='portrait', papertype=None, format='png',
+        transparent=True, bbox_inches='tight', pad_inches=0
+    )
+plt.close('all')
+###############################################################################
+# Mossy Infected Plot
+###############################################################################
+infct = infected / max(infected)
+(fig, ax) = plt.subplots(figsize=(10, 5.5), sharex=True)
+STYLE = {
+    "width": .35, "alpha": .5, "dpi": 1500, "legend": True,
+    "aspect": .25, "xRange": [0, (365*10)],
+    "yRange": (0, 1)
+}
+STYLE['aspect'] = monet.scaleAspect(.125, STYLE)
+colors = ('#3772ffAF', )
+ax.plot(range(0, len(infct)), infct, lw=STYLE['width'], ls='--', color=colors[0])
+ax.set_xlim(STYLE['xRange'][0], STYLE['xRange'][1])
+ax.set_ylim(STYLE['yRange'][0], STYLE['yRange'][1])
+ax.set_aspect(aspect=STYLE["aspect"])
+ax.axes.xaxis.set_ticklabels([])
+ax.axes.yaxis.set_ticklabels([])
+ax.axes.xaxis.set_visible(False)
+ax.axes.yaxis.set_visible(False)
+ax.set_axis_off()
+fig.savefig(
+        "{}/{}.png".format(PT_IMG, 'Infected'),
         dpi=STYLE['dpi'], facecolor=None, edgecolor='w',
         orientation='portrait', papertype=None, format='png',
         transparent=True, bbox_inches='tight', pad_inches=0
--- PYF/ONE/PYF_videoSet.sh
+++ PYF/ONE/PYF_videoSet.sh
@@ -3,6 +3,7 @@
 # argv1: User
 # argv2: AOI
 
+bash ./PYF_video.sh $1 $2 E_016_020_100_000_000
 bash ./PYF_video.sh $1 $2 E_016_024_050_000_000
 bash ./PYF_video.sh $1 $2 E_016_024_100_000_000
 bash ./PYF_video.sh $1 $2 E_016_024_150_000_000
