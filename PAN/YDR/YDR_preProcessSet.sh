b707486d5ea02acc056284f6d86bf8bbcb0cf6c9
chipdelmal <chipdelmal@gmail.com>
2021-04-07 16:04:57 -0700

    YDR server sync

--- PAN/YDR/YDR_preProcessSet.sh
+++ PAN/YDR/YDR_preProcessSet.sh
@@ -2,8 +2,8 @@
 
 # argv1: USR
 
-bash ./YDR_preProcess.sh $1 ASD
-bash ./YDR_preProcess.sh $1 XSD
-bash ./YDR_preProcess.sh $1 YSD
+# bash ./YDR_preProcess.sh $1 ASD
+# bash ./YDR_preProcess.sh $1 XSD
+# bash ./YDR_preProcess.sh $1 YSD
 bash ./YDR_preProcess.sh $1 AXS
 bash ./YDR_preProcess.sh $1 YXS
