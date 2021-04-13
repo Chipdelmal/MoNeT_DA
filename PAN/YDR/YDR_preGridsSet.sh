deee05c16f4736e5f74d686a4e90334b804197b6
chipdelmal <chipdelmal@gmail.com>
2021-04-13 14:14:58 -0700

    YDR server sync

--- PAN/YDR/YDR_preGridsSet.sh
+++ PAN/YDR/YDR_preGridsSet.sh
@@ -2,8 +2,9 @@
 
 # argv1: User
 
-python YDR_preGrids.py $1 homing ASD
-python YDR_preGrids.py $1 homing XSD
-python YDR_preGrids.py $1 homing YSD
+# python YDR_preGrids.py $1 homing ASD
+# python YDR_preGrids.py $1 homing XSD
+# python YDR_preGrids.py $1 homing YSD
 python YDR_preGrids.py $1 shredder AXS
 python YDR_preGrids.py $1 shredder YXS
+python YDR_preGrids.py $1 shredder CRS
