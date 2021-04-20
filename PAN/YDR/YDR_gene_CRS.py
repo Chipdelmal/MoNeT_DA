1b25f2e3a006b01d738ac3b55de574a595260ffa
chipdelmal <chipdelmal@gmail.com>
2021-04-20 08:15:01 -0700

    YDR Fixing CRS counts

--- PAN/YDR/YDR_gene_CRS.py
+++ PAN/YDR/YDR_gene_CRS.py
@@ -18,7 +18,7 @@ genotypes = (
 ###############################################################################
 ECO_DICT = OrderedDict((
     ('W',   (('W', locusF), )),
-    ('H',   (('P', locusF), )),
+    ('H',   (('H', locusF), )),
     ('B',   (('B', locusF), )),
     ('R',   (('R', locusF), ))
 ))
@@ -31,7 +31,7 @@ HLT_DICT = OrderedDict((
     ('H*', (('H', locusF), )),
     ('O-', (('W', locusF), ('B', locusF), ('R', locusF)))
 ))
-CRS_HLT = monet.geneFrequencies(HLT_DICT, genotypes)
+CRS_HLT = monet.carrierFrequencies(HLT_DICT, genotypes)
 
 ###############################################################################
 # Trash genotype counts
@@ -40,7 +40,7 @@ TRS_DICT = OrderedDict((
     ('C*', (('H', locusF), )),
     ('O-', (('W', locusF), ('B', locusF), ('R', locusF)))
 ))
-CRS_TRS = monet.geneFrequencies(TRS_DICT, genotypes)
+CRS_TRS = monet.carrierFrequencies(TRS_DICT, genotypes)
 
 ###############################################################################
 # Wild genotype counts
@@ -49,7 +49,7 @@ WLD_DICT = OrderedDict((
     ('O*', (('B', locusF), ('R', locusF), ('H', locusF))),
     ('W-', (('W', locusF), ))
 ))
-CRS_WLD = monet.geneFrequencies(WLD_DICT, genotypes)
+CRS_WLD = monet.carrierFrequencies(WLD_DICT, genotypes)
 
 ###############################################################################
 # Drive Selector
