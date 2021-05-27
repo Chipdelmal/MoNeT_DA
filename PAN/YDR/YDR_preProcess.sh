c4b9fba9d609c96b2eb63f9fe78b4caf711e1323
chipdelmal <chipdelmal@gmail.com>
2021-05-27 07:56:15 -0700

    YDR server merge

--- PAN/YDR/YDR_preProcess.sh
+++ PAN/YDR/YDR_preProcess.sh
@@ -4,10 +4,10 @@
 # argv2: Gene Drive
 
 expSet=$(./YDR_selector.sh $2)
-python YDR_preProcess.py $1 $expSet $2 ECO
-python YDR_preProcess.py $1 $expSet $2 HLT
+# python YDR_preProcess.py $1 $expSet $2 ECO
+# python YDR_preProcess.py $1 $expSet $2 HLT
 python YDR_preProcess.py $1 $expSet $2 TRS
-python YDR_preProcess.py $1 $expSet $2 WLD
+# python YDR_preProcess.py $1 $expSet $2 WLD
 
 if [ "$3" = "True" ]; then
     ./YDR_preTraces.sh "$@"
