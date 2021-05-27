82083f353b9685dd6c0589595f50f9be4f8ae4c2
chipdelmal <chipdelmal@gmail.com>
2021-05-27 11:20:15 -0700

    QLD Minor tweaks for validation

--- AUS/QLD/QLD_preProcess.py
+++ AUS/QLD/QLD_preProcess.py
@@ -14,7 +14,7 @@ from more_itertools import locate
 # os.system("taskset -p 0xff %d" % os.getpid())
 
 if monet.isNotebook():
-    (USR, AOI, LND) = ('dsk', 'HLT', '10')
+    (USR, AOI, LND) = ('dsk2', 'HLT', '10')
     JOB = aux.JOB_DSK
 else:
     (USR, AOI, LND) = (sys.argv[1], sys.argv[2], sys.argv[3])
@@ -65,13 +65,13 @@ for exp in EXPS:
             [(i in expIDPreDone) for i in expIDForProcessing], 
             lambda x: x!=True
         ))
-        expIter = [xpIter[i] for i in expsIxList]
+        expIter = [expIter[i] for i in expsIxList]
     sys.stdout.write("\033[K")
     ###########################################################################
     # Process data
     ###########################################################################
     Parallel(n_jobs=JOB)(
-        delayed(dbg.preProcessParallel)(
+        delayed(monet.preProcessParallel)(
             exIx, expNum, gene,
             analysisOI=AOI, prePath=PT_PRE, nodesAggLst=land,
             fNameFmt='{}/{}-{}_', MF=drv.maleFemaleSelector(AOI),
@@ -80,3 +80,6 @@ for exp in EXPS:
             REP=aux.REP, SRP=aux.SRP
         ) for exIx in expIter
     )
+
+import compress_pickle as pkl
+dta = pkl.load('/home/chipdelmal/Documents/WorkSims/QLD/Experiments/s1/PREPROCESS/E_001-HLT_01_srp.bz')
