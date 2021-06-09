97d0315423e9ffed32bf91c5d22572bdf71706da
chipdelmal <chipdelmal@gmail.com>
2021-06-09 11:11:22 -0700

    SDP Updating for new experiments

--- PAN/SDP/SDP_gene.py
+++ PAN/SDP/SDP_gene.py
@@ -2,10 +2,14 @@
 import MoNeT_MGDrivE as monet
 import SDP_gene_AXS as AXS
 import SDP_gene_CRS as CRS
+import SDP_gene_CRS as CRX
+import SDP_gene_CRS as CRY
 import SDP_gene_FSR as FSR
 import SDP_gene_IIT as IIT
 import SDP_gene_PGS as PGS
 import SDP_gene_SDR as SDR
+import SDP_gene_SDR as SDX
+import SDP_gene_SDR as SDY
 import SDP_gene_SIT as SIT
 
 ###############################################################################
@@ -13,16 +17,24 @@ import SDP_gene_SIT as SIT
 ###############################################################################
 def driveSelector(DRV, AOI, popSize=10000):
     # Drive Selection ---------------------------------------------------------
-    if DRV == 'AXS':
-        (aggD, yRange, folder) = AXS.driveParameters(AOI, popSize)
-    if DRV == 'CRS':
-        (aggD, yRange, folder) = CRS.driveParameters(AOI, popSize)
-    if DRV == 'FSR':
-        (aggD, yRange, folder) = FSR.driveParameters(AOI, popSize)
     if DRV == 'IIT':
         (aggD, yRange, folder) = IIT.driveParameters(AOI, popSize)
+    if DRV == 'FSR':
+        (aggD, yRange, folder) = FSR.driveParameters(AOI, popSize)
     if DRV == 'PGS':
         (aggD, yRange, folder) = PGS.driveParameters(AOI, popSize)
+    if DRV == 'AXS':
+        (aggD, yRange, folder) = AXS.driveParameters(AOI, popSize)
+    if DRV == 'CRX':
+        (aggD, yRange, folder) = CRX.driveParameters(AOI, popSize)
+    if DRV == 'CRY':
+        (aggD, yRange, folder) = CRY.driveParameters(AOI, popSize)
+    if DRV == 'SDX':
+        (aggD, yRange, folder) = SDX.driveParameters(AOI, popSize)
+    if DRV == 'SDY':
+        (aggD, yRange, folder) = SDY.driveParameters(AOI, popSize)
+    if DRV == 'CRS':
+        (aggD, yRange, folder) = CRS.driveParameters(AOI, popSize)
     if DRV == 'SDR':
         (aggD, yRange, folder) = SDR.driveParameters(AOI, popSize)
     if DRV == 'SIT':
---
+++ PAN/SDP/SDP_gene_CRY.py
@@ -0,0 +1,76 @@
+
+from collections import OrderedDict
+import MoNeT_MGDrivE as monet
+
+###############################################################################
+# https://github.com/Chipdelmal/MGDrivE/blob/master/MGDrivE/R/Cube-ASmidler.R
+###############################################################################
+
+genotypes = (
+    'WWWW','PWWW','MWWW','WGWW','PGWW','MGWW','WRWW','PRWW','MRWW','WBWW','PBWW',
+    'MBWW','MWPW','PWWG','MGPW','PWWR','MRPW','PWWB','MBPW','MWWG','MWPG','MWWR',
+    'MWPR','MWWB','MWPB','WGWG','PGWG','MGWG','WGWR','PRWG','MRWG','WBWG','PBWG',
+    'MBWG','MGPG','PGWR','MRPG','PGWB','MBPG','MGWR','MGPR','MGWB','MGPB','WRWR',
+    'PRWR','MRWR','WBWR','PBWR','MBWR','MRPR','PRWB','MBPR','MRWB','MRPB','WBWB',
+    'PBWB','MBWB','MBPB'
+)
+(locusA, locusB, locusF) = ((0, 2), (1, 3), (0, 1, 2, 3))
+
+###############################################################################
+# Ecology genotype counts
+###############################################################################
+ECO_DICT = OrderedDict((
+    ('WA',  (('W', locusA), )),
+    ('P',   (('P', locusA), )),
+    ('M',   (('M', locusA), )),
+    ('WB',  (('W', locusB), )),
+    ('G',   (('G', locusB), )),
+    ('B',   (('B', locusB), )),
+    ('R',   (('R', locusB), ))
+))
+CRS_ECO = monet.geneFrequencies(ECO_DICT, genotypes)
+
+###############################################################################
+# Health genotype counts
+###############################################################################
+HLT_DICT = OrderedDict((
+    ('H*', (('G', locusB), )),
+    ('O-', (('W', locusB), ('B', locusB), ('R', locusB)))
+))
+CRS_HLT = monet.geneFrequencies(HLT_DICT, genotypes)
+
+###############################################################################
+# Trash genotype counts
+###############################################################################
+TRS_DICT = OrderedDict((
+    ('C*', (('P', locusA), ('M', locusA))),
+    ('O-', (('W', locusA), ))
+))
+CRS_TRS = monet.geneFrequencies(TRS_DICT, genotypes)
+
+###############################################################################
+# Wild genotype counts
+###############################################################################
+WLD_DICT = OrderedDict((
+    ('O*', (('B', locusB), ('R', locusB), ('G', locusB))),
+    ('W-', (('W', locusB), ))
+))
+CRS_WLD = monet.geneFrequencies(WLD_DICT, genotypes)
+
+###############################################################################
+# Drive Selector
+###############################################################################
+def driveParameters(TYPE, popSize):
+    if TYPE == 'ECO':
+        aggD = monet.generateAggregationDictionary(*CRS_ECO)
+        yRange = popSize*2
+    elif TYPE == 'HLT':
+        aggD = monet.generateAggregationDictionary(*CRS_HLT)
+        yRange = popSize/2
+    elif TYPE == 'TRS':
+        aggD = monet.generateAggregationDictionary(*CRS_TRS)
+        yRange = popSize
+    elif TYPE == 'WLD':
+        aggD = monet.generateAggregationDictionary(*CRS_WLD)
+        yRange = popSize
+    return (aggD, yRange, 'CRISPR-DsY')
