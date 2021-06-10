12e0e07fefcea210de6a6d7e9e9aa9af91962a7b
chipdelmal <chipdelmal@gmail.com>
2021-06-10 15:21:46 -0700

    STP Synching to test on server

---
+++ STP/RBC/STP_dtaTraces.py
@@ -0,0 +1,69 @@
+
+import sys
+from os import path
+from joblib import dump, load
+from datetime import datetime
+import pandas as pd
+import MoNeT_MGDrivE as monet
+import STP_aux as aux
+import STP_gene as drv
+import STP_land as lnd
+import STP_auxDebug as dbg
+from joblib import Parallel, delayed
+import warnings
+warnings.filterwarnings("ignore",category=UserWarning)
+
+if monet.isNotebook():
+    (USR, LND, AOI, QNT) = ('dsk', 'PAN', 'HLT', '50')
+else:
+    (USR, LND, AOI, QNT) = (
+        sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4]
+    )
+FNAME = 'DTA_FLTR.csv'
+# Setup number of cores -------------------------------------------------------
+if USR=='dsk':
+    JOB = aux.JOB_DSK
+else:
+    JOB = aux.JOB_SRV
+###############################################################################
+# Paths
+###############################################################################
+EXPS = aux.getExps(LND)
+(drive, land) = (
+    drv.driveSelector(aux.DRV, AOI, popSize=aux.POP_SIZE),
+    lnd.landSelector(EXPS[0], LND)
+)
+(PT_ROT, PT_IMG, PT_DTA, PT_PRE, PT_OUT, PT_MTR) = aux.selectPath(USR, EXPS[0], LND)
+PT_OUT = path.join(path.split(path.split(PT_ROT)[0])[0], 'ML')
+PT_IMG = PT_IMG + 'dtaTraces/'
+[monet.makeFolder(i) for i in [PT_OUT, PT_IMG]]
+PT_SUMS = [path.join(PT_ROT, exp, 'SUMMARY') for exp in EXPS]
+###############################################################################
+# Read Iter
+###############################################################################
+expsIter = load(path.join(PT_OUT, 'DTA_PST.job'))
+###########################################################################
+# Style
+###########################################################################
+(CLR, YRAN) = (drive.get('colors'), (0, drive.get('yRange')))
+STYLE = {
+    "width": .5, "alpha": .5, "dpi": 100, "legend": True,
+    "aspect": 1, "colors": CLR, "xRange": aux.XRAN, "yRange": YRAN
+}
+###############################################################################
+# Plot
+###############################################################################
+(fNum, digs) = monet.lenAndDigits(expsIter)
+Parallel(n_jobs=JOB)(
+    delayed(dbg.exportPstTracesParallel)(
+        exIx, fNum,
+        aux.STABLE_T, 0, QNT, STYLE, PT_IMG, 
+        digs=digs, border=True, autoAspect=True, labelPos=(.8, .2)
+    ) for exIx in expsIter
+)
+# Export gene legend ------------------------------------------------------
+repDta = pkl.load(repFiles[-1])
+monet.exportGeneLegend(
+    repDta['genotypes'], [i[:-2]+'cc' for i in CLR], 
+    PT_IMG+'/legend_{}.png'.format(AOI), 500
+)
