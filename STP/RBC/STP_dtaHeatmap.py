2b3eab7b1bde9b3211e66a372dd0a71be0cf552f
chipdelmal <chipdelmal@gmail.com>
2021-06-14 16:37:37 -0700

    STP dtaHeatmap minor fixes an heatmaps

--- STP/RBC/STP_dtaHeatmap.py
+++ STP/RBC/STP_dtaHeatmap.py
@@ -66,8 +66,9 @@ thsStr = str(int(float(aux.THS)*100))
 )
 DATA = pd.read_csv(path.join(PT_OUT, fName_I))
 # (zmin, zmax) = (min(DATA[MOI])*2, max(DATA[MOI]))
-(zmin, zmax) = (.4, max(DATA[MOI]))
-(lvls, mthd) = (np.arange(zmin, zmax, (zmax-zmin)/20), 'linear')
+(zmin, zmax) = (-0.1, max(DATA[MOI]))
+# (lvls, mthd) = (np.arange(zmin*.9, zmax*1.1, (zmax-zmin)/20), 'linear')
+(lvls, mthd) = (np.arange(-0.1, 1.1, 1.5/20), 'nearest')
 # Filter the dataframe --------------------------------------------------------
 headerInd = [i for i in DATA.columns if i[0]=='i']
 uqVal = {i: list(DATA[i].unique()) for i in headerInd}
@@ -75,58 +76,81 @@ uqVal = {i: list(DATA[i].unique()) for i in headerInd}
 # Filter dataframe
 ###############################################################################
 fltr = {
-    'i_sex': 3,
+    'i_sex': 2,
+    'i_ren': 2,
+    'i_res': .2,
     'i_rsg': 0,
-    'i_gsv': 1e-02,
-    'i_fcf': 1,
+    'i_gsv': 0,
+    'i_fcf': 1.5,
     'i_mfm': 0.73,
     'i_mft': 0.93,
     'i_hrm': 0.611,
     'i_hrt': 0.916,
     'i_grp': 0, 'i_mig': 0
 }
-ks = [all(i) for i in zip(*[DATA[k]==fltr[k] for k in list(fltr.keys())])]
-dfSrf = DATA[ks]
-dfSrf.shape
-###############################################################################
-# Generate Surface
-###############################################################################
-(x, y, z) = (dfSrf[HD_IND[0]], dfSrf[HD_IND[1]], dfSrf[MOI])
-rs = monet.calcResponseSurface(x, y, z, scalers=[1, 1, 1], mthd=mthd)
-# Get ranges ------------------------------------------------------------------
-(a, b) = ((min(x), max(x)), (min(y), max(y)))
-(rsG, rsS) = (rs['grid'], rs['surface'])
-# Plot the response surface ---------------------------------------------------
-(fig, ax) = plt.subplots(figsize=(10, 8))
-# Experiment points, contour lines, response surface
-xy = ax.plot(rsG[0], rsG[1], 'k.', ms=3, alpha=.25, marker='.')
-cc = ax.contour(rsS[0], rsS[1], rsS[2], levels=lvls, colors='w', linewidths=.5, alpha=.25)
-cs = ax.contourf(rsS[0], rsS[1], rsS[2], levels=lvls, cmap=monet.cmapM, extend='max')
-# Styling ---------------------------------------------------------------------
-cbar = fig.colorbar(cs)
-cbar.ax.get_yaxis().labelpad = 25
-cbar.ax.set_ylabel('{}'.format(MOI), fontsize=15, rotation=270)
-ax.grid(which='both', axis='y', lw=.1, alpha=0.1, color=(0, 0, 0))
-ax.grid(which='minor', axis='x', lw=.1, alpha=0.1, color=(0, 0, 0))
-plt.xlim(a[0], a[1])
-plt.ylim(b[0], b[1])
-pTitle = ' '.join(['[{}: {}]'.format(i, fltr[i]) for i in fltr])
-plt.title(pTitle, fontsize=7.5)
-###############################################################################
-# Export File
-###############################################################################
-# Generate filename -----------------------------------------------------------
-(allKeys, fltrKeys) = (list(aux.DATA_SCA.keys()), set(fltr.keys()))
-fElements = []
-for (i, k) in enumerate(allKeys):
-    if k in fltrKeys:
-        xEl = str(int(fltr[k]*aux.DATA_SCA[k])).zfill(aux.DATA_PAD[k])
-    else:
-        xEl = 'X'*aux.DATA_PAD[k]
-    fElements.append(xEl)
-fName = 'E_'+'_'.join(fElements)
-# Save file -------------------------------------------------------------------
-fig.savefig(
-    path.join(PT_IMG, fName+'.png'), 
-    dpi=500, bbox_inches='tight', pad=0
-)
+[fltr.pop(i) for i in HD_IND]
+# Sweep over values -----------------------------------------------------------
+kSweep = 'i_rsg'
+sweep = uqVal[kSweep]
+for sw in sweep:
+    fltr[kSweep] = sw
+    ks = [all(i) for i in zip(*[DATA[k]==fltr[k] for k in list(fltr.keys())])]
+    dfSrf = DATA[ks]
+    if dfSrf.shape[0] == 0:
+        continue
+    ###############################################################################
+    # Generate Surface
+    ###############################################################################
+    (x, y, z) = (dfSrf[HD_IND[0]], dfSrf[HD_IND[1]], dfSrf[MOI])
+    rs = monet.calcResponseSurface(
+        x, y, z, 
+        scalers=[max(DATA[HD_IND[0]]), max(DATA[HD_IND[1]]), 1], mthd=mthd,
+        xAxis=xSca, yAxis=ySca,
+        DXY=(ngdx, ngdy)
+    )
+    # Get ranges ------------------------------------------------------------------
+    (a, b) = ((min(x), max(x)), (min(y), max(y)))
+    (rsG, rsS) = (rs['grid'], rs['surface'])
+    # Plot the response surface ---------------------------------------------------
+    (fig, ax) = plt.subplots(figsize=(10, 8))
+    # Experiment points, contour lines, response surface
+    xy = ax.plot(rsG[0], rsG[1], 'k.', ms=3, alpha=.25, marker='.')
+    cc = ax.contour(rsS[0], rsS[1], rsS[2], levels=lvls, colors='w', linewidths=.5, alpha=.25)
+    cs = ax.contourf(rsS[0], rsS[1], rsS[2], levels=lvls, cmap=cmap, extend='max')
+    # Styling ---------------------------------------------------------------------
+    cbar = fig.colorbar(cs)
+    cbar.ax.get_yaxis().labelpad = 25
+    cbar.ax.set_ylabel('{}'.format(MOI), fontsize=15, rotation=270)
+    ax.grid(which='both', axis='y', lw=.1, alpha=0.1, color=(0, 0, 0))
+    ax.grid(which='minor', axis='x', lw=.1, alpha=0.1, color=(0, 0, 0))
+    ax.set_xlabel(HD_IND[0])
+    ax.set_ylabel(HD_IND[1])
+    ax.axes.xaxis.set_ticklabels(dfSrf[HD_IND[0]].unique())
+    ax.axes.yaxis.set_ticklabels(dfSrf[HD_IND[1]].unique())
+    plt.xlim(0, 1)# a[0], a[1])
+    plt.ylim(0, 1)# b[0], b[1])
+    pTitle = ' '.join(['[{}: {}]'.format(i, fltr[i]) for i in fltr])
+    plt.title(pTitle, fontsize=7.5)
+    ###############################################################################
+    # Export File
+    ###############################################################################
+    # Generate filename -----------------------------------------------------------
+    (allKeys, fltrKeys) = (list(aux.DATA_SCA.keys()), set(fltr.keys()))
+    fElements = []
+    for (i, k) in enumerate(allKeys):
+        if k in fltrKeys:
+            xEl = str(int(fltr[k]*aux.DATA_SCA[k])).zfill(aux.DATA_PAD[k])
+        else:
+            xEl = 'X'*aux.DATA_PAD[k]
+        fElements.append(xEl)
+    fName = 'E_'+'_'.join(fElements)
+    # Save file -------------------------------------------------------------------
+    fig.savefig(
+        path.join(PT_IMG, fName+'.png'), 
+        dpi=500, bbox_inches='tight', pad=0
+    )
+    # Clearing and closing (fig, ax) ----------------------------------------------
+    plt.clf()
+    plt.cla() 
+    plt.close(fig)
+    plt.gcf()
