a3820360490c2eb76b579c664936a2acb33cd4b5
chipdelmal <chipdelmal@gmail.com>
2021-06-22 09:39:26 -0700

    STP server sync

--- STP/RBC/STP_dtaTraces.sh
+++ STP/RBC/STP_dtaTraces.sh
@@ -7,9 +7,9 @@
 # Reshape and generate output file
 ###############################################################################
 python STP_dtaExplore.py $1 PAN HLT 50
-python STP_dtaConverted.py $1 PAN HLT 50 $2
+python STP_dtaConverter.py $1 PAN HLT 50 $2
 ###############################################################################
 # Export traces file
 ###############################################################################
 python STP_dtaTraces.py $1 PAN HLT 50 $2
-python STP_dtaTraces.py $1 PAN ECO 50 $2
+# python STP_dtaTraces.py $1 PAN ECO 50 $2
