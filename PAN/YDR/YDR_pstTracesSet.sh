5e1fc2d6043ec93444d76ddbae0ddb41cb7d4680
chipdelmal <chipdelmal@gmail.com>
2021-09-16 10:40:38 -0700

    YDR pstTracesSet update

--- PAN/YDR/YDR_pstTracesSet.sh
+++ PAN/YDR/YDR_pstTracesSet.sh
@@ -5,11 +5,11 @@
 
 
 QNT="50"
-bash ./YDR_pstTraces.sh $1 ASD $2
-bash ./YDR_pstTraces.sh $1 YSD $2
-bash ./YDR_pstTraces.sh $1 AXS $2
-bash ./YDR_pstTraces.sh $1 YXS $2
-bash ./YDR_pstTraces.sh $1 XSD $2
-bash ./YDR_pstTraces.sh $1 CRS $2
+bash ./YDR_pstTraces.sh $1 ASD $2 '0.1'
+bash ./YDR_pstTraces.sh $1 YSD $2 '0.1'
+bash ./YDR_pstTraces.sh $1 AXS $2 '0.1'
+bash ./YDR_pstTraces.sh $1 YXS $2 '0.1'
+bash ./YDR_pstTraces.sh $1 XSD $2 '0.1'
+bash ./YDR_pstTraces.sh $1 CRS $2 '0.1'
 
 # bash ./YDR_pstGridsSet.sh $1
