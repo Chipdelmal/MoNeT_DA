8d6a014fb749af8fd7c38d0fb674d6f92c226969
chipdelmal <chipdelmal@gmail.com>
2021-09-17 08:32:39 -0700

    STP Update to preTraces

--- STP/RBC/STP_preTraces.sh
+++ STP/RBC/STP_preTraces.sh
@@ -2,10 +2,11 @@
 
 # argv1: User
 # argv2: Land
+# argv3: Drive
 
-python STP_preTraces.py $1 ECO $2
-python STP_preTraces.py $1 HLT $2
-python STP_preTraces.py $1 WLD $2
-python STP_preTraces.py $1 TRS $2
+python STP_preTraces.py $1 ECO $2 $3
+python STP_preTraces.py $1 HLT $2 $3
+python STP_preTraces.py $1 WLD $2 $3
+python STP_preTraces.py $1 TRS $2 $3
 
 python STP_preGrids.py $1 $2
