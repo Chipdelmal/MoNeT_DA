3c163994fe57d22dacd7d6f98157b598cba0f5b3
chipdelmal <chipdelmal@gmail.com>
2021-10-06 11:48:43 -0700

    TRP Normalizations and plotting are fixed

--- NET/TRP/TRP_dev.py
+++ NET/TRP/TRP_dev.py
@@ -1,4 +1,5 @@
 
+import math
 import numpy as np
 from os import path
 import networkx as nx
@@ -27,7 +28,8 @@ sitesNum = sites.shape[0]
 ###############################################################################
 # Delete self-transitions -----------------------------------------------------
 np.fill_diagonal(migMat, 0)
-psiN = normalize(migMat, axis=1, norm='l2')
+psiN = normalize(migMat, axis=1, norm='l1')
+np.apply_along_axis(sum, 1, psiN)
 # Generate initial migration network ------------------------------------------
 G = nx.from_numpy_matrix(psiN)
 G.remove_edges_from(nx.selfloop_edges(G))
@@ -47,12 +49,12 @@ traps = np.asarray([
 # Calculate trapping probabilities
 ###############################################################################
 trapDists = np.asarray([
-    [np.linalg.norm(site-trap) for site in sites]
+    [math.dist(trap, site) for site in sites]
     for trap in traps
 ]).T
 # Traps attractiveness --------------------------------------------------------
 trapProbs = np.asarray([
-    [aux.trapProbability(i, b=0.1) for i in dist] 
+    [aux.trapProbability(i, A=.5, b=.75) for i in dist] 
     for dist in trapDists
 ])
 # Traps identity --------------------------------------------------------------
@@ -67,35 +69,46 @@ trapEscape = np.asarray([
 ###############################################################################
 (n, m) = (sitesNum, TRAPS_NUM)
 tau = np.empty((n+m, n+m))
-# BB Section --------------------------------------------------------------
+# BB Section ------------------------------------------------------------------
 BB = psiN
 for i in range(n):
     for j in range(n):
         tau[i, j] = BB[i, j]
-# BQ Section --------------------------------------------------------------
+# BQ Section ------------------------------------------------------------------
 BQ = trapProbs.T
 for i in range(n):
     for j in range(m):
         tau[i, j + n] = BQ[j, i]
-# QB Section --------------------------------------------------------------
+# QB Section ------------------------------------------------------------------
 QB = trapEscape.T
 for i in range(n):
     for j in range(m):
         tau[j+n, i] = QB[i, j]
-# QQ Section --------------------------------------------------------------
+# QQ Section ------------------------------------------------------------------
 QQ = trapIdentity
 for i in range(m):
     for j in range(m):
         tau[i+n, j+n] = QQ[i, j]
+# Normalize -------------------------------------------------------------------
+tauN = normalize(tau, axis=1, norm='l1')
+np.apply_along_axis(sum, 1, tauN)
 ###############################################################################
-# Normalize and plot matrix
+# Plot matrix
 ###############################################################################
-tauN = normalize(tau, axis=1, norm='l2')
 # plt.imshow(tau, vmax=1e-2, interpolation='nearest')
 (fig, ax) = plt.subplots(figsize=(15, 15))
-# ax.imshow(tauN, vmax=1e-2, cmap='Purples', interpolation='nearest')
-# fig.savefig('./out.png', dpi=500)
-(fig, ax) = aux.plotNetwork(fig, ax, tauN, traps, sites, [0], c='#2B62F7')
-(fig, ax) = aux.plotNetwork(fig, ax, tauN, sites, sites, [0], c='#FB3264')
-plt.scatter(sites.T[0], sites.T[1], marker='^')
-plt.scatter(traps.T[0], traps.T[1], marker='o')
+plt.imshow(tauN, vmax=1e-1, cmap='Purples', interpolation='nearest')
+fig.savefig('./out.png', dpi=500)
+###############################################################################
+# Plot landscape
+###############################################################################
+BBN = tauN[:sitesNum, :sitesNum]
+BQN = tauN[:sitesNum, sitesNum:]
+(LW, ALPHA, SCA) = (.125, .15, 50)
+(fig, ax) = plt.subplots(figsize=(15, 15))
+(fig, ax) = aux.plotNetwork(fig, ax, BQN*SCA, traps, sites, [0], c='#f72585', lw=LW, alpha=ALPHA)
+(fig, ax) = aux.plotNetwork(fig, ax, tauN*SCA, sites, sites, [0], c='#2B62F7', lw=LW, alpha=ALPHA)
+plt.scatter(sites.T[0], sites.T[1], marker='^', color='#ef233c99', s=25)
+plt.scatter(traps.T[0], traps.T[1], marker='o', color='#03045e99', s=25)
+fig.savefig('./trapsNetwork.png', dpi=500)
+
