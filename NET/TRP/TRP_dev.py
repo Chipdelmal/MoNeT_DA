9ee186bf929e34b29db9f7b5028a588b42175720
chipdelmal <chipdelmal@gmail.com>
2021-10-13 11:41:17 -0700

    TRP Further refactoring of the code

--- NET/TRP/TRP_dev.py
+++ NET/TRP/TRP_dev.py
@@ -10,9 +10,7 @@ import seaborn as sns
 from sklearn.preprocessing import normalize
 from sklearn.cluster import AgglomerativeClustering
 import TRP_aux as aux
-import random
-
-random.seed(10)
+import TRP_fun as fun
 
 TRAPS_NUM = 20
 (PT_DTA, PT_IMG, EXP_FNAME) = (
@@ -20,6 +18,10 @@ TRAPS_NUM = 20
     '/Volumes/marshallShare/Mov/trp',
     '300'
 )
+kPars = {
+    'Trap': {'A': 0.5, 'b': 0.75},
+    'Escape': {'A': 0, 'b': 100}
+}
 ###############################################################################
 # Read migration matrix and pop sites
 ############################################################################### 
@@ -30,76 +32,24 @@ sitesNum = sites.shape[0]
 ###############################################################################
 # Transitions Matrix and Base Netowrk
 ###############################################################################
-# Delete self-transitions -----------------------------------------------------
-np.fill_diagonal(migMat, 0)
-psiN = normalize(migMat, axis=1, norm='l1')
-np.apply_along_axis(sum, 1, psiN)
-# Generate initial migration network ------------------------------------------
-G = nx.from_numpy_matrix(psiN)
-G.remove_edges_from(nx.selfloop_edges(G))
+psiN = fun.deleteLoopsFromMatrix(migMat)
+# np.apply_along_axis(sum, 1, psiN)
 ###############################################################################
-# Generate some trap locations
+# Generate some trap locations and calculate distances
 ###############################################################################
 (minX, minY) = np.apply_along_axis(min, 0, sites)
 (maxX, maxY) = np.apply_along_axis(max, 0, sites)
-# Locate traps random-uniformly across the landscape --------------------------
-traps = np.asarray([
-    [
-        np.random.uniform(minX, maxX, 1)[0], 
-        np.random.uniform(minY, maxY, 1)[0]
-    ] for i in range(TRAPS_NUM)
-])
+traps = fun.placeTrapsRanUnif(TRAPS_NUM, (minX, maxX), (minY, maxY))
+trapDists = fun.calcTrapToSitesDistance(traps, sites)
 ###############################################################################
-# Calculate trapping probabilities
+# Calculate trapping probabilities and assemble matrix
 ###############################################################################
-trapDists = np.asarray([
-    [math.dist(trap, site) for site in sites]
-    for trap in traps
-]).T
-# Traps attractiveness --------------------------------------------------------
-trapProbs = np.asarray([
-    [aux.trapProbability(i, A=.5, b=.75) for i in dist] 
-    for dist in trapDists
-])
-# Traps identity --------------------------------------------------------------
-trapIdentity = np.identity(TRAPS_NUM)
-# Traps escape ----------------------------------------------------------------
-trapEscape = np.asarray([
-    [aux.trapProbability(i, b=10) for i in dist] 
-    for dist in trapDists
-]).T
-###############################################################################
-# Assemble matrix
-###############################################################################
-(n, m) = (sitesNum, TRAPS_NUM)
-tau = np.empty((n+m, n+m))
-# BB Section ------------------------------------------------------------------
-BB = psiN
-for i in range(n):
-    for j in range(n):
-        tau[i, j] = BB[i, j]
-# BQ Section ------------------------------------------------------------------
-BQ = trapProbs.T
-for i in range(n):
-    for j in range(m):
-        tau[i, j + n] = BQ[j, i]
-# QB Section ------------------------------------------------------------------
-QB = trapEscape.T
-for i in range(n):
-    for j in range(m):
-        tau[j+n, i] = QB[i, j]
-# QQ Section ------------------------------------------------------------------
-QQ = trapIdentity
-for i in range(m):
-    for j in range(m):
-        tau[i+n, j+n] = QQ[i, j]
-# Normalize -------------------------------------------------------------------
-tauN = normalize(tau, axis=1, norm='l1')
+tProbs = fun.calcTrapsSections(trapDists, params=kPars)
+tauN = fun.assembleTrapMigration(psiN, tProbs)
 np.apply_along_axis(sum, 1, tauN)
 ###############################################################################
 # Plot matrix
 ###############################################################################
-# plt.imshow(tau, vmax=1e-2, interpolation='nearest')
 (fig, ax) = plt.subplots(figsize=(15, 15))
 plt.imshow(tauN, vmax=1e-1, cmap='Purples', interpolation='nearest')
 fig.savefig(
---
+++ NET/TRP/TRP_fun.py
@@ -0,0 +1,90 @@
+
+import math
+import numpy as np
+import networkx as nx
+from sklearn.preprocessing import normalize
+
+
+def expTrapProbability(dist, A=1, b=1):
+    prob = A * math.exp(-b * dist)
+    return prob
+
+
+def deleteLoopsFromMatrix(migMat):
+    zmat = np.copy(migMat)
+    np.fill_diagonal(zmat, 0)
+    psiN = normalize(zmat, axis=1, norm='l1')
+    return psiN
+
+
+def placeTrapsRanUnif(trapsNumber, xMinMax, yMinMax):
+    traps = np.asarray([
+        [
+            np.random.uniform(xMinMax[0], xMinMax[1], 1)[0], 
+            np.random.uniform(yMinMax[0], yMinMax[1], 1)[0]
+        ] for i in range(trapsNumber)
+    ])
+    return traps
+
+
+def calcTrapToSitesDistance(trapsXY, sitesXY, dFun=math.dist):
+    trapDists = np.asarray([
+        [dFun(trap, site) for site in sitesXY]
+        for trap in trapsXY
+    ]).T
+    return trapDists
+
+
+def calcTrapsSections(
+        trapDists, 
+        tFun=expTrapProbability, 
+        params={
+            'Trap':   {'A': .5, 'b': .75},
+            'Escape': {'A': 0, 'b': 100},
+        }
+    ):
+    trapProbs = np.asarray([
+        [tFun(i, **params['Trap']) for i in dist] 
+        for dist in trapDists
+    ])
+    trapIdentity = np.identity(trapDists.shape[0])
+    trapEscape = np.asarray([
+        [tFun(i, **params['Escape']) for i in dist] 
+        for dist in trapDists
+    ]).T
+    # Assemble dictionary
+    retDict = {
+        'Trap': trapProbs, 
+        'Identity': trapIdentity,
+        'Escape': trapEscape
+    }
+    return retDict
+
+
+def assembleTrapMigration(psiN, tProbs):
+    (n, m) = (psiN.shape[0], tProbs['Trap'].shape[1])
+    tau = np.empty((n+m, n+m))
+    # Migration Section -------------------------------------------------------
+    B = psiN
+    for i in range(n):
+        for j in range(n):
+            tau[i, j] = B[i, j]
+    # Trapped Section ---------------------------------------------------------
+    A = tProbs['Trap'].T
+    for i in range(n):
+        for j in range(m):
+            tau[i, j + n] = A[j, i]
+    # Escape Section ----------------------------------------------------------
+    O = tProbs['Escape'].T
+    for i in range(n):
+        for j in range(m):
+            tau[j+n, i] = O[i, j]
+    # Identity Section --------------------------------------------------------
+    I = tProbs['Identity']
+    for i in range(m):
+        for j in range(m):
+            tau[i+n, j+n] = I [i, j]
+    # Normalize ---------------------------------------------------------------
+    tauN = normalize(tau, axis=1, norm='l1')
+    np.apply_along_axis(sum, 1, tauN)
+    return tauN
