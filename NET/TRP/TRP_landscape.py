474c3ccb2bc6dbdc09d2d094b1f04ccb13613380
chipdelmal <chipdelmal@gmail.com>
2021-10-21 14:21:57 -0700

    TRP Two point-types model is running

--- NET/TRP/TRP_landscape.py
+++ NET/TRP/TRP_landscape.py
@@ -5,23 +5,21 @@ import numpy.random as rand
 import MoNeT_MGDrivE as monet
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import normalize
+import TRP_aux as aux
 
-POINTS = 50
+POINTS = 25
 (xRan, yRan) = ((-10, 10), (-10, 10))
 PTS_TMAT = np.asarray([
-    [.1, .85, .05],
-    [.8, .2, 0],
-    [1, 0, 0]
+    [0.05, 0.95],
+    [0.20, 0.80]
 ])
 # PTS_TMAT = np.asarray([[1]])
 ###############################################################################
 # Generate pointset
 ###############################################################################
-coords = list(zip(
-    rand.uniform(*xRan, POINTS), 
-    rand.uniform(*yRan, POINTS)
-))
+coords = list(zip(rand.uniform(*xRan, POINTS), rand.uniform(*yRan, POINTS)))
 pTypes = rand.randint(0, PTS_TMAT.shape[0], POINTS)
+sites = np.asarray(coords)
 ###############################################################################
 # Generate matrices
 ###############################################################################
@@ -48,3 +46,21 @@ tauN = normalize(msk*tau, axis=1, norm='l1')
 plt.matshow(tau, vmax=.05)
 plt.matshow(msk)
 plt.matshow(tauN, vmax=.05)
+###############################################################################
+# Plot Landscape
+###############################################################################
+(LW, ALPHA, SCA) = (.125, .5, 50)
+mrkrs = ('X', 'o', "^")
+(fig, ax) = plt.subplots(figsize=(15, 15))
+for (i, site) in enumerate(sites):
+    plt.scatter(
+        site[0], site[1], 
+        marker=mrkrs[pTypes[i]], color='#03045eDB', 
+        s=250, zorder=20, edgecolors='w', linewidths=2
+    )
+ax.set_xlim(*xRan)
+ax.set_ylim(*yRan)
+(fig, ax) = aux.plotNetwork(
+    fig, ax, tauN*SCA, sites, sites, [1]*len(coords), 
+    c='#03045e', lw=LW, alpha=ALPHA, arrows=False
+)
