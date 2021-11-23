e1f9ca86654f86e09a97d5c2ad5809abeabfe2b4
chipdelmal <chipdelmal@gmail.com>
2021-11-22 16:40:41 -0800

    TPT Adding paths to aux

---
+++ PAN/TPT/TPT_aux.py
@@ -0,0 +1,155 @@
+
+import re
+import random
+import matplotlib
+import numpy as np
+import pandas as pd
+from glob import glob
+import MoNeT_MGDrivE as monet
+
+
+# #############################################################################
+# Constants
+# #############################################################################
+OVW = True
+REL_START = 100
+(JOB_DSK, JOB_SRV) = (8, 40)
+(POP_SIZE, XRAN, FZ) = (
+    1e7*1.25, # 2e6*1.5/2, 
+    (0, 10*int(365)), 
+    True
+)
+(STABLE_T, MLR, SAMP_RATE) = (0, False, 2)
+(XP_ID, DRV, XP_PTRN, NO_REL_PAT) = (
+    'STP', 'LDR/SDR',
+    'E_{}_{}_{}_{}_{}_{}_{}_{}_{}_{}-{}_{}_{}.{}', '00'
+)
+(SUM, AGG, SPA, REP, SRP) = (True, False, False, False, True)
+(DATA_NAMES, DATA_PRE, DATA_PST) = (
+    ('TTI', 'TTO', 'WOP', 'RAP', 'MNX', 'POE', 'CPT', 'DER'),
+    ('ECO', 'HLT', 'TRS', 'WLD'), ('HLT', 'TRS', 'WLD')
+)
+REF_FILE = 'E_01_00_00000_000000000000_000000000000_0000000_0000000_0000000_0000000_0000000'
+# Data Analysis ---------------------------------------------------------------
+(DATA_HEAD, DATA_SCA, DATA_PAD) = (
+    (
+        ('i_sex', 1),   ('i_ren', 2),   ('i_res', 3), 
+        ('i_rsg', 4),   ('i_gsv', 5), 
+        ('i_fch', 6),   ('i_fcb', 7),   ('i_fcr', 8),
+        ('i_hrm', 9),   ('i_hrf', 10), 
+        ('i_grp', 12)
+    ),
+    {
+        'i_sex': 1e0,   'i_ren': 1e0,   'i_res': 1e3, 
+        'i_rsg': 1e10,  'i_gsv': 1e10,  
+        'i_fch': 1e5,   'i_fcb': 1e5,   'i_fcr': 1e5,
+        'i_hrm': 1e5,   'i_hrf': 1e5, 
+        'i_grp': 1e0,   'i_mig': 1e5
+    },
+    {
+        'i_sex': 2,     'i_ren': 2,     'i_res': 5,   
+        'i_rsg': 12,    'i_gsv': 12,  
+        'i_fch': 7,     'i_fcb': 7,     'i_fcr': 7,
+        'i_hrm': 7,     'i_hrf': 7,   
+        'i_grp': 2,     'i_mig': 6
+    }
+)
+(THI, THO, THW, TAP) = (
+    [.10, .25, .50],
+    [.10, .25, .50],
+    [.10, .25, .50],
+    [0, 365]# [int(i) for i in range(0, XRAN[1], int(XRAN[1]/5))]
+)
+
+# #############################################################################
+# Dependent Variables for Heatmaps
+# #############################################################################
+def selectDepVars(MOI, AOI):
+    # Select ranges and dependent variable-------------------------------------
+    if (MOI == 'WOP') or (MOI == 'TTO'):
+        scalers = [1, 1, XRAN]
+        (HD_DEP, IND_RAN) = ('0.1', 7)
+    elif (MOI == 'TTI'):
+        scalers = [1, 1, XRAN]
+        (HD_DEP, IND_RAN) = ('0.1', 7)
+    elif (MOI == 'RAP'):
+        scalers = [1, 100, 90]
+        (HD_DEP, IND_RAN) = ('486', 7)
+    elif (MOI == 'MNX'):
+        scalers = [1, 100, 1]
+        (HD_DEP, IND_RAN) = ('min', 7)
+    elif (MOI == 'POE'):
+        scalers = [1, 100, 1]
+        (HD_DEP, IND_RAN) = ('POE', 1) 
+    elif (MOI == 'CPT'):
+        scalers = [1, 100, 1]
+        (HD_DEP, IND_RAN) = ('CPT', 1) 
+    elif (MOI == 'DER'):
+        scalers = [1, 100, 5]
+        (HD_DEP, IND_RAN) = ('DER', 5) 
+    elif (MOI == 'MNF'):
+        scalers = [1, 1, 1]
+        (HD_DEP, IND_RAN) = ('MNF', 1) 
+    # Color Mapping -----------------------------------------------------------
+    if MOI == 'WOP':
+        cmap = monet.cmapP
+    elif MOI == 'CPT':
+        cmap = monet.cmapM
+    elif MOI == 'POE':
+        cmap = monet.cmapC
+    else:
+        cmap = monet.cmapW
+    return (scalers, HD_DEP, IND_RAN, cmap)
+
+# #############################################################################
+# Experiments
+# #############################################################################
+def getExps():
+    return ('X10000', 'X2500', 'X5000', 'X75000')
+
+# #############################################################################
+# Names and patterns
+# #############################################################################
+def patternForReleases(ren, AOI, ftype, ext='bz'):
+    strPat = XP_PTRN.format(
+        '*', ren, '*', '*', '*', 
+        '*', '*', '*', '*', '*', 
+        AOI, '*', ftype, ext
+    )
+    return strPat
+
+
+def splitExpNames(PATH_OUT, ext='bz'):
+    out = [i.split('/')[-1].split('-')[0] for i in glob(PATH_OUT+'*.'+ext)]
+    return sorted(list(set(out)))
+
+
+def getExperimentsIDSets(PATH_EXP, skip=-1, ext='.bz'):
+    filesList = glob(PATH_EXP+'E*')
+    fileNames = [i.split('/')[-1].split('.')[-2] for i in filesList]
+    splitFilenames = [re.split('_|-', i)[:skip] for i in fileNames]
+    ids = []
+    for c in range(len(splitFilenames[0])):
+        colSet = set([i[c] for i in splitFilenames])
+        ids.append(sorted(list(colSet)))
+    return ids
+
+
+# #############################################################################
+# Paths and Style
+# #############################################################################
+def selectPath(USR, EXP, LND, DRV=None):
+    if USR == 'srv':
+        PATH_ROOT = '/RAID5/marshallShare/TP13/{}/'.format(EXP)
+    elif USR == 'lab':
+        PATH_ROOT = '/Volumes/marshallShare/TP13/{}/'.format(EXP)
+    (PATH_IMG, PATH_DATA) = (
+            '{}img/'.format(PATH_ROOT), '{}'.format(PATH_ROOT)
+        )
+    PATH_PRE = PATH_DATA + 'PREPROCESS/'
+    PATH_OUT = PATH_DATA + 'POSTPROCESS/'
+    PATH_MTR = PATH_DATA + 'SUMMARY/'
+    fldrList = [PATH_ROOT, PATH_IMG, PATH_DATA, PATH_PRE, PATH_OUT, PATH_MTR]
+    [monet.makeFolder(i) for i in fldrList]
+    return (PATH_ROOT, PATH_IMG, PATH_DATA, PATH_PRE, PATH_OUT, PATH_MTR)
+
---
+++ PAN/TPT/TPT_preProcess.py
@@ -0,0 +1,81 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+from datetime import datetime
+import MoNeT_MGDrivE as monet
+from joblib import Parallel, delayed
+from more_itertools import locate
+import TPT_aux as aux
+import TPT_gene as drv
+
+
+if monet.isNotebook():
+    (USR, AOI, DRV) = ('lab', 'HLT', 'LDR')
+    JOB = aux.JOB_DSK
+else:
+    (USR, AOI, DRV) = (sys.argv[1], sys.argv[2], sys.argv[3])
+    JOB = aux.JOB_SRV
+###############################################################################
+# Processing loop
+###############################################################################
+EXPS = aux.getExps(LND)
+exp = EXPS[0]
+for exp in EXPS:
+    ###########################################################################
+    # Setting up paths
+    ###########################################################################
+    (drive, land) = (
+        drv.driveSelector(DRV, AOI, popSize=aux.POP_SIZE),
+        lnd.landSelector(exp, LND, USR=USR)
+    )
+    (gene, fldr) = (drive.get('gDict'), drive.get('folder'))
+    (PT_ROT, PT_IMG, PT_DTA, PT_PRE, PT_OUT, PT_MTR) = aux.selectPath(
+        USR, exp, LND, DRV
+    )
+    # Time and head -----------------------------------------------------------
+    tS = datetime.now()
+    monet.printExperimentHead(
+        PT_DTA, PT_PRE, tS, 
+        '{} PreProcess [{}:{}:{}]'.format(aux.XP_ID, fldr, exp, AOI)
+    )
+    ###########################################################################
+    # Load folders
+    ###########################################################################
+    fmtStr = '{}* Create files list...{}'
+    print(fmtStr.format(monet.CBBL, monet.CEND), end='\r')
+    (expDirsMean, expDirsTrac) = monet.getExpPaths(
+        PT_DTA, mean='ANALYZED/', reps='TRACE/'
+    )
+    # print(land)
+    (expNum, nodeDigits) = (len(expDirsMean), 2)
+    expIter = list(zip(list(range(expNum)), expDirsMean, expDirsTrac))
+    # Check for potential miss-matches in experiments folders -----------------
+    (meanFNum, tracFNum) = (len(expDirsMean), len(expDirsTrac))
+    if (meanFNum != tracFNum):
+        errorString = 'Unequal experiments folders lengths ({}/{})'
+        sys.exit(errorString.format(meanFNum, tracFNum)) 
+    # Check for pre-existing files and skip if needed -------------------------
+    if aux.OVW == False:
+        expIDPreDone = set(monet.splitExpNames(PT_PRE))
+        expIDForProcessing = [i.split('/')[-1] for i in expDirsMean]
+        expsIxList = list(locate(
+            [(i in expIDPreDone) for i in expIDForProcessing],
+            lambda x: x!=True
+        ))
+        expIter = [expIter[i] for i in expsIxList]
+    sys.stdout.write("\033[K")
+    ###########################################################################
+    # Process data
+    ###########################################################################
+    Parallel(n_jobs=JOB)(
+        delayed(dbg.preProcessParallel)(
+            exIx, expNum, gene,
+            analysisOI=AOI, prePath=PT_PRE, nodesAggLst=land,
+            fNameFmt='{}/{}-{}_', MF=drv.maleFemaleSelector(AOI),
+            cmpr='bz2', nodeDigits=2,
+            SUM=aux.SUM, AGG=aux.AGG, SPA=aux.SPA,
+            REP=aux.REP, SRP=aux.SRP
+        ) for exIx in expIter
+    )
