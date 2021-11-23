ac23e325cd7fa4ebb6ead85db3eb25ad7a25658e
chipdelmal <chipdelmal@gmail.com>
2021-11-22 16:06:41 -0800

    TRP Adding donut landscape

--- NET/TRP/TRP_landscape.py
+++ NET/TRP/TRP_landscape.py
@@ -1,5 +1,6 @@
 
 import math
+import random
 import numpy as np
 from os import path
 import pandas as pd
@@ -10,9 +11,9 @@ from sklearn.preprocessing import normalize
 import TRP_aux as aux
 
 
-(LND, MOD) = ('Unif', 'HOM')
+(LND, MOD) = ('Donut', 'HET')
 if monet.isNotebook():
-    (POINTS, EXP_FNAME) = (100, 'SQR_02')
+    (POINTS, EXP_FNAME) = (100, 'DNT_02')
     (PT_DTA, PT_GA, PT_IMG) = aux.selectPaths('dsk')
 else:
     POINTS = argv[1]
@@ -44,6 +45,19 @@ if LND == 'Grid':
     y = np.linspace(yRan[0], yRan[1], int((yRan[1]-yRan[0])/2))
     coords = np.asarray(np.meshgrid(x, y)).T
     coords = np.concatenate(coords)
+elif LND == 'Donut':
+    coords = []
+    for i in range(POINTS):
+        radius = (8, 10)
+        center = (0, 0)
+        # random angle
+        alpha = 2 * math.pi * random.random()
+        # random radius
+        r = rand.uniform(radius[0], radius[1])
+        # calculating coordinates
+        x = r * math.cos(alpha) + center[0]
+        y = r * math.sin(alpha) + center[1]
+        coords.append([x, y])
 else:
     coords = list(zip(rand.uniform(*xRan, POINTS), rand.uniform(*yRan, POINTS)))
 # Point-types -----------------------------------------------------------------
