a1094a25fadae4488c541be962f21fcaa5ca20d9
chipdelmal <chipdelmal@gmail.com>
2021-11-29 10:08:35 -0800

    TPT Testing run on server

---
+++ PAN/TPT/TPT_pstFraction.py
@@ -0,0 +1,118 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import sys
+import numpy as np
+from glob import glob
+import TPT_aux as aux
+import TPT_gene as drv
+from datetime import datetime
+import compress_pickle as pkl
+import MoNeT_MGDrivE as monet
+from more_itertools import locate
+from joblib import Parallel, delayed
+
+
+if monet.isNotebook():
+    (USR, AOI, DRV) = ('lab', 'HUM', 'LDR')
+else:
+    (USR, AOI, DRV) = (sys.argv[1], sys.argv[2], sys.argv[3])
+# Setup number of threads -----------------------------------------------------
+JOB=aux.JOB_DSK
+if USR == 'srv':
+    JOB = aux.JOB_SRV
+###############################################################################
+# Processing loop
+###############################################################################
+EXPS = aux.getExps()
+exp = EXPS[0]
+for exp in EXPS:
+    # #########################################################################
+    # Setup paths and drive
+    # #########################################################################
+    (drive, land) = (
+        drv.driveSelector(DRV, AOI, popSize=aux.POP_SIZE, humSize=aux.HUM_SIZE),
+        aux.landSelector(USR=USR)
+    )
+    (gene, fldr) = (drive.get('gDict'), drive.get('folder'))
+    (PT_ROT, PT_IMG, PT_DTA, PT_PRE, PT_OUT, PT_MTR) = aux.selectPath(
+        USR, exp, DRV
+    )
+    tS = datetime.now()
+    monet.printExperimentHead(
+        PT_PRE, PT_OUT, tS, 
+        aux.XP_ID+' PstFraction [{}:{}:{}]'.format(fldr, exp, AOI)
+    )
+    # #########################################################################
+    # Base experiments
+    #   These are the experiments without any releases (for fractions)
+    # #########################################################################
+    # Get releases number set -------------------------------------------------
+    ren = aux.getExperimentsIDSets(PT_PRE, skip=-1)[2]
+    # Get base experiments pattern --------------------------------------------
+    if GRID_REF:
+        basePat = aux.patternForReleases(aux.NO_REL_PAT, AOI, 'sum')
+        baseFiles = sorted(glob(PT_PRE+basePat))
+        baseFNum = len(baseFiles)
+    # #########################################################################
+    # Probe experiments
+    #   sum: Analyzed data aggregated into one node
+    #   srp: Garbage data aggregated into one node
+    # #########################################################################
+    (xpNum, digs) = monet.lenAndDigits(ren)
+    (i, rnIt) = (0, '12')
+    for (i, rnIt) in enumerate(ren):
+        monet.printProgress(i+1, xpNum, digs)
+        # Repetitions data (Garbage) ------------------------------------------
+        tracePat = aux.patternForReleases(rnIt, AOI, 'srp')
+        traceFiles = sorted(glob(PT_PRE+tracePat))
+        # Mean data (Analyzed) ------------------------------------------------
+        meanPat = aux.patternForReleases(rnIt, AOI, 'sum')
+        meanFiles = sorted(glob(PT_PRE+meanPat))
+        expNum = len(meanFiles)
+        # Patch for static reference file -------------------------------------
+        if not GRID_REF:
+            baseFiles = [
+                dbg.replaceExpBase(f, aux.REF_FILE) for f in meanFiles
+            ]
+            baseFNum = len(baseFiles)
+        # Create experiments iterator list ------------------------------------
+        expIter = list(zip(
+            list(range(expNum)), baseFiles, meanFiles, traceFiles
+        ))
+        # Check for potential miss-matches in experiments folders -------------
+        (meanFNum, tracFNum) = (len(meanFiles), len(traceFiles))
+        if (meanFNum!=tracFNum) or (baseFNum!=meanFNum) or (baseFNum!=tracFNum):
+            errorString = 'Unequal experiments folders lengths ({}/{}/{})'
+            sys.exit(errorString.format(baseFNum, meanFNum, tracFNum)) 
+        # Filter existing if needed -------------------------------------------
+        if aux.OVW == False:
+            expIDPreDone = set(monet.splitExpNames(PT_OUT, ext='npy'))
+            expIDForProcessing = [i.split('/')[-1][:-14] for i in meanFiles]
+            expsIxList = list(locate(
+                [(i in expIDPreDone) for i in expIDForProcessing], 
+                lambda x: x!=True
+            ))
+            expIter = [expIter[i] for i in expsIxList]
+        # #####################################################################
+        # Process data
+        # #####################################################################
+        Parallel(n_jobs=JOB)(
+            delayed(dbg.pstFractionParallel)(
+                exIx, PT_OUT
+            ) for exIx in expIter
+        )
+
+# (drive, land) = (
+#     drv.driveSelector(DRV, AOI, popSize=aux.POP_SIZE),
+#     lnd.landSelector(exp, LND, USR=USR)
+# )
+# (gene, fldr) = (drive.get('gDict'), drive.get('folder'))
+# (PT_ROT, PT_IMG, PT_DTA, PT_PRE, PT_OUT, PT_MTR) = aux.selectPath(
+#     USR, exp, LND, DRV
+# )
+# dta = pkl.load(
+#     PT_PRE+
+#     'E_01_00_00000_000000000000_000000000000_0000000_0000000_0000000_0000000_0000000-HLT_00_sum.bz'
+# )['population']
+# print(dta)
