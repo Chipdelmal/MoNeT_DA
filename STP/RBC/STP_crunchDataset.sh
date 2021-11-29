1ca43bd23d21f0380124161bbf040bffb7bf698c
chipdelmal <chipdelmal@gmail.com>
2021-11-29 10:12:09 -0800

    STP Server sync

--- STP/RBC/STP_crunchDataset.sh
+++ STP/RBC/STP_crunchDataset.sh
@@ -11,22 +11,22 @@ THS='0.1'
 ###############################################################################
 # python STP_preProcess.py $1 'HLT' $LND $DRV
 # python STP_pstFraction.py $1 'HLT' $LND $DRV
-python STP_pstProcess.py $1 'HLT' $LND $DRV $QNT
+# python STP_pstProcess.py $1 'HLT' $LND $DRV $QNT
 ###############################################################################
 # Generate dataframes
 ###############################################################################
-python STP_clsCompile.py $USR $LND 'HLT' $DRV $QNT 'TTI'
-python STP_clsCompile.py $USR $LND 'HLT' $DRV $QNT 'TTO'
-python STP_clsCompile.py $USR $LND 'HLT' $DRV $QNT 'WOP'
-python STP_clsCompile.py $USR $LND 'HLT' $DRV $QNT 'CPT'
-python STP_clsCompile.py $USR $LND 'HLT' $DRV $QNT 'POE'
-python STP_clsCompile.py $USR $LND 'HLT' $DRV $QNT 'MNX'
+#python STP_clsCompile.py $USR $LND 'HLT' $DRV $QNT 'TTI'
+#python STP_clsCompile.py $USR $LND 'HLT' $DRV $QNT 'TTO'
+#python STP_clsCompile.py $USR $LND 'HLT' $DRV $QNT 'WOP'
+#python STP_clsCompile.py $USR $LND 'HLT' $DRV $QNT 'CPT'
+#python STP_clsCompile.py $USR $LND 'HLT' $DRV $QNT 'POE'
+#python STP_clsCompile.py $USR $LND 'HLT' $DRV $QNT 'MNX'
 ###############################################################################
 # Unify dataframes
 ###############################################################################
-python STP_clsUnify.py $USR $LND 'HLT' $DRV $QNT
-python STP_dtaAmend.py $USR $LND 'HLT' $DRV $QNT
-# python STP_clsPreprocess.py $USR $LND 'HLT' $DRV $QNT
+#python STP_clsUnify.py $USR $LND 'HLT' $DRV $QNT
+#python STP_dtaAmend.py $USR $LND 'HLT' $DRV $QNT
+#python STP_clsPreprocess.py $USR $LND 'HLT' $DRV $QNT
 if [[ "$DCE" = "True" ]]; then
     python STP_dtaDICE.py $USR $LND 'HLT' $DRV $QNT
 fi
