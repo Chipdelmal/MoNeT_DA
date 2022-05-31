f99e5ffdf7c028d56d4874a9382eb78e2dbe766b
chipdelmal <chipdelmal@gmail.com>
2022-05-31 13:03:11 -0700

    tGD Pushing changes on the padding

--- PAN/tGD/tGD_preCrunch.sh
+++ PAN/tGD/tGD_preCrunch.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
-
 USR=$1
 
-# source tGD_preProcess.sh $USR linkedDrive
-# source tGD_preTraces.sh $USR linkedDrive
-# python tGD_overlays.py $USR linkedDrive
-
-# source tGD_preProcess.sh $USR splitDrive
-# source tGD_preTraces.sh $USR splitDrive
-# python tGD_overlays.py $USR splitDrive
-
+###############################################################################
+# Linked Drive
+###############################################################################
+source tGD_preProcess.sh $USR linkedDrive
+source tGD_preTraces.sh $USR linkedDrive
+python tGD_overlays.py $USR linkedDrive
+###############################################################################
+# Split Drive
+###############################################################################
+source tGD_preProcess.sh $USR splitDrive
+source tGD_preTraces.sh $USR splitDrive
+python tGD_overlays.py $USR splitDrive
+###############################################################################
+# tGD
+###############################################################################
 source tGD_preProcess.sh $USR tGD
 source tGD_preTraces.sh $USR tGD
 python tGD_overlays.py $USR tGD
