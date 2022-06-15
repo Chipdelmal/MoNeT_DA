31a2a92b15b4301d2fbe3c88a8baf092b873d334
chipdelmal <chipdelmal@gmail.com>
2022-06-15 16:36:13 -0700

    tGD Adapting dta traces

--- PAN/tGD/tGD_dtaTraces.sh
+++ PAN/tGD/tGD_dtaTraces.sh
@@ -6,6 +6,14 @@ THS=$3
 AOI=$4
 PLT=$5
 
-python tGD_dtaExplore.py $USR 'linkedDrive' $AOI $QNT $THS
+python tGD_dtaExploreMulti.py $USR 'linkedDrive' $AOI $QNT $THS
 python tGD_dtaConverter.py $USR 'linkedDrive' $AOI $QNT $THS 'NM' $PLT
 python tGD_dtaTraces.py $USR 'linkedDrive' $AOI $QNT $THS $PLT
+
+python tGD_dtaExploreMulti.py $USR 'splitDrive' $AOI $QNT $THS
+python tGD_dtaConverter.py $USR 'splitDrive' $AOI $QNT $THS 'NM' $PLT
+python tGD_dtaTraces.py $USR 'splitDrive' $AOI $QNT $THS $PLT
+
+python tGD_dtaExploreMulti.py $USR 'tGD' $AOI $QNT $THS
+python tGD_dtaConverter.py $USR 'tGD' $AOI $QNT $THS 'NM' $PLT
+python tGD_dtaTraces.py $USR 'tGD' $AOI $QNT $THS $PLT
