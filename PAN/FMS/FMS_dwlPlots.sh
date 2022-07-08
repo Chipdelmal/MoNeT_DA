7bda51c247a92c11705895df5751829a13e8e259
chipdelmal <chipdelmal@gmail.com>
2022-07-08 09:19:32 -0700

    FMS Improvements on download routine

--- PAN/FMS/FMS_dwlPlots.sh
+++ PAN/FMS/FMS_dwlPlots.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-for drv in "ifegenia_3" "ifegenia_4" "ifegenia_5" "pgSIT" # "IIT" "RDL" "RDF" 
+for drv in "ifegenia_3" "ifegenia_4" "ifegenia_5" "pgSIT" # "IIT" "RIDL" "fsRIDL" 
 do
-    scp -r "lab:/RAID5/marshallShare/fem_pgSIT/${drv}/ML/img" "/home/chipdelmal/Documents/WorkSims/fem_pgSIT/${drv}"
+    scp -r "lab:/RAID5/marshallShare/fem_pgSIT/${drv}/ML/img/heat" "/home/chipdelmal/Documents/WorkSims/fem_pgSIT/${drv}/"
     scp -r "lab:/RAID5/marshallShare/fem_pgSIT/${drv}/img/dtaTraces" "/home/chipdelmal/Documents/WorkSims/fem_pgSIT/${drv}"
 done 
