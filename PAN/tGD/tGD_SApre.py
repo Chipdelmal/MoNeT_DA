ab67105dfb0c456db066b09956ee88a266a9d2e9
chipdelmal <chipdelmal@gmail.com>
2022-08-22 12:26:55 -0700

    tGD Exploring SALib

--- PAN/tGD/tGD_SApre.py
+++ PAN/tGD/tGD_SApre.py
@@ -1,22 +1,31 @@
 
+from os import path
 import pandas as pd
 from SALib.sample import saltelli
+import tGD_aux as aux
 
 
 SAMPLES_NUM = 1024
-VARS = ['fcs', 'fcb', 'fga', 'fgb', 'cut', 'hdr']
+VARS = ['fcs', 'fcb', 'fga', 'fgb', 'cut', 'hdr', 'res']
+(USR, DRV) = ('srv2', 'linkedDrive')
+exp = '100'
+###############################################################################
+# Setup Paths
+###############################################################################
+(PT_ROT, PT_IMG, PT_DTA, PT_PRE, PT_OUT, PT_MTR) = aux.selectPath(USR, DRV, exp)
 ###############################################################################
 # Model Inputs
 ###############################################################################
 problem = {
-    'num_vars': 6,
+    'num_vars': len(VARS),
     'names': VARS, 
     'bounds': [
         [0, 1], [0, 1],
         [0, 1], [0, 1],
-        [0, 1], [0, 1]
+        [0, 1], [0, 1],
+        [0, 1]
     ],
-    'dists': ['unif', 'unif', 'unif', 'unif', 'unif', 'unif']
+    'dists': ['unif']*len(VARS)
 }
 ###############################################################################
 # Sampling
@@ -25,4 +34,5 @@ param_values = saltelli.sample(problem, SAMPLES_NUM)
 ###############################################################################
 # Assemble and Export
 ###############################################################################
-pd.DataFrame(param_values, columns=VARS)
+df = pd.DataFrame(param_values, columns=VARS)
+df.to_csv(path.join(PT_DTA, 'SA_pre.csv'))
