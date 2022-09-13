ac02ab35123ad07140608581cc6c7dc4d9381619
chipdelmal <chipdelmal@gmail.com>
2022-09-13 13:39:23 -0700

    PGS pre/pst Crunch both running

--- PAN/PGS/PGS_pstCrunch.sh
+++ PAN/PGS/PGS_pstCrunch.sh
@@ -3,7 +3,14 @@
 USR=$1
 DRV=$2
 AOI=$3
-QNT='50'
-
+QNT=$4
+###############################################################################
+# Setup Path
+###############################################################################
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+cd "$parent_path"
+###############################################################################
+# Launch Scripts
+###############################################################################
 python PGS_pstFraction.py $USR $DRV $AOI
-python PGS_pstProcess.py $URS $DRV $AOI $QNT
+python PGS_pstProcess.py $USR $DRV $AOI $QNT
--- PAN/PGS/PGS_pstProcess.py
+++ PAN/PGS/PGS_pstProcess.py
@@ -16,6 +16,7 @@ from joblib import Parallel, delayed
 if monet.isNotebook():
     (USR, DRV, AOI, QNT) = ('srv', 'PGS', 'HLT', '50')
 else:
+    print(sys.argv[1:])
     (USR, DRV, AOI, QNT) = sys.argv[1:]
 GRID_REF = False
 # Setup number of threads -----------------------------------------------------
