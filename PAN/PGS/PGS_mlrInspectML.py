3547ad3b4edb6c2f9be2146288452eed02d07807
chipdelmal <chipdelmal@gmail.com>
2022-10-06 16:16:49 -0700

    PGS Importance plots added

--- PAN/PGS/PGS_mlrInspectML.py
+++ PAN/PGS/PGS_mlrInspectML.py
@@ -9,6 +9,7 @@ from datetime import datetime
 import matplotlib.pyplot as plt
 from matplotlib.collections import PatchCollection
 from matplotlib.patches import Rectangle
+import matplotlib.patches as mpatches
 from treeinterpreter import treeinterpreter as ti
 import MoNeT_MGDrivE as monet
 import PGS_aux as aux
@@ -16,7 +17,7 @@ import PGS_gene as drv
 
 
 if monet.isNotebook():
-    (USR, DRV, AOI, THS, MOI) = ('srv', 'PGS', 'HLT', '0.1', 'CPT')
+    (USR, DRV, AOI, THS, MOI) = ('srv', 'PGS', 'HLT', '0.1', 'WOP')
 else:
     (USR, DRV, AOI, THS, MOI) = sys.argv[1:]
 # Setup number of threads -----------------------------------------------------
@@ -52,8 +53,8 @@ rf = pkl.load(path.join(PT_OUT, fName))
 # Evaluate ML
 ###############################################################################
 probeX = (
-    ('ren', 48),
-    ('rer', 48),
+    ('ren', 24),
+    ('rer', 20),
     ('rei', 8),
     ('pct', 0.0),
     ('pmd', 1.0),
@@ -64,7 +65,6 @@ probeX = (
 FEATS = [i[0] for i in probeX]
 # Evaluate models at probe point ----------------------------------------------
 vct = np.array([[i[1] for i in probeX]])
-# pred = rf.predict(vct)
 (prediction, bias, contributions) = ti.predict(rf, vct)
 # -----------------------------------------------------------------------------
 print("* Predicted: {}".format(prediction[0][0]))
@@ -72,13 +72,44 @@ print("\tBias: {}".format(bias[0]))
 for (c, feature, inVal) in zip(contributions[0], FEATS, vct[0]):
     ptest = '{:.4f}'.format(c).zfill(3)
     print('\t{} ({:.2f}): {}'.format(feature, inVal, ptest))
-
-
+###############################################################################
+# Plot evaluation
+###############################################################################
+CLR = ('#3b28ccAA', '#f72585AA')
 rWidth = 1
-(bias, contributions) = (bias, contributions)
-
-(bs, cntr) = (bias[0], contributions[0])
-(fig, ax) = plt.subplots(figsize=(10, 10))
-ax.add_patch(Rectangle((bias, 0), bs+cntr[0], 1))
-ax.set_xlim(-2, 2)
-ax.set_ylim(0, 4)
+# Figure function -------------------------------------------------------------
+(bs, cntr, vals) = (bias[0], contributions[0], vct[0])
+(x, yTicks) = (bs, [])
+(fig, ax) = plt.subplots(figsize=(10, 2.5))
+for (ix, v) in enumerate(cntr):
+    col = (CLR[0] if (v>=0) else CLR[1])
+    y = rWidth*ix
+    # Shape -------------------------------------------------------------------
+    rectangle = Rectangle((x, y), v, rWidth, color=col, ec='#00000088')
+    # Add patch ---------------------------------------------------------------
+    ax.add_patch(rectangle)
+    yHalf = y+(rWidth)/2
+    x = x + v
+    yTicks.append(yHalf)
+    ax.text(
+        1800, yHalf, '{:.2f} '.format(v), 
+        va='center', ha='right', fontsize=8
+    )
+    ax.text(
+        0, yHalf, ' {:.2f}'.format(vals[ix]), 
+        va='center', ha='left', fontsize=8
+    )
+# Add labels and ticks --------------------------------------------------------
+ax.set_yticks(yTicks)
+ax.set_yticklabels(FEATS)
+ax.vlines(
+    bias, 0, 1, 
+    transform=ax.get_xaxis_transform(), 
+    ls='--', lw=.5, colors='#efc3e6'
+)
+ax.vlines(
+    x, 0, y, 
+    ls='--', lw=1, colors='#a663cc'
+)
+ax.set_xlim(0, 1800)
+ax.set_ylim(0, rWidth*len(cntr))
