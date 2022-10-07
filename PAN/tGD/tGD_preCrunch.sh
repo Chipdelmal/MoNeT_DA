c3532531bc0d4be36c6a2b0c1925edc9873f1741
chipdelmal <chipdelmal@gmail.com>
2022-10-07 12:54:47 -0700

    TPT Changes for traces (y range)

--- PAN/tGD/tGD_preCrunch.sh
+++ PAN/tGD/tGD_preCrunch.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 USR=$1
 
-###############################################################################
-# Linked Drive
-###############################################################################
-source tGD_preProcess.sh $USR linkedDrive
-source tGD_preTraces.sh $USR linkedDrive
-python tGD_overlays.py $USR linkedDrive
-###############################################################################
-# Split Drive
-###############################################################################
-source tGD_preProcess.sh $USR splitDrive
-source tGD_preTraces.sh $USR splitDrive
-python tGD_overlays.py $USR splitDrive
+# ###############################################################################
+# # Linked Drive
+# ###############################################################################
+# source tGD_preProcess.sh $USR linkedDrive
+# source tGD_preTraces.sh $USR linkedDrive
+# python tGD_overlays.py $USR linkedDrive
+# ###############################################################################
+# # Split Drive
+# ###############################################################################
+# source tGD_preProcess.sh $USR splitDrive
+# source tGD_preTraces.sh $USR splitDrive
+# python tGD_overlays.py $USR splitDrive
 ###############################################################################
 # tGD
 ###############################################################################
