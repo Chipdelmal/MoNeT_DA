af436a5df5a6f5100893aee9a4c47dc3841dfb33
chipdelmal <chipdelmal@gmail.com>
2022-10-13 15:30:07 -0700

    FMS clsCrunch update

--- PAN/FMS/FMS_clsCrunch.sh
+++ PAN/FMS/FMS_clsCrunch.sh
@@ -11,29 +11,29 @@ THS=$5
 ###############################################################################
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd "$parent_path"
-# ###############################################################################
-# # Launch Scripts
-# ###############################################################################
-# for mtr in "WOP" "CPT" "POE" "TTI" "TTO" "MNX"
-# do
-#     python FMS_clsCompile.py $USR $DRV $QNT $AOI $mtr
-# done
-# python FMS_clsUnify.py $USR $DRV $QNT $AOI $THS 
-# ###############################################################################
-# # Launch Scripts (ML)
-# ###############################################################################
-# for mtr in "WOP" "CPT" "TTI" "TTO" "MNX"
-# do
-#     python FMS_clsCompileML.py $USR $DRV $AOI $mtr
-# done
-# python FMS_clsUnifyML.py $USR $DRV $AOI $THS 
-# ###############################################################################
-# # Launch Scripts (SA)
-# ###############################################################################
-# for mtr in "WOP" "CPT" "POE" "TTI" "TTO"
-# do
-#     python FMS_saAnalyzer.py $USR $DRV $QNT $AOI $THS $mtr
-# done
+###############################################################################
+# Launch Scripts
+###############################################################################
+for mtr in "WOP" "CPT" "POE" "TTI" "TTO" "MNX"
+do
+    python FMS_clsCompile.py $USR $DRV $QNT $AOI $mtr
+done
+python FMS_clsUnify.py $USR $DRV $QNT $AOI $THS 
+###############################################################################
+# Launch Scripts (ML)
+###############################################################################
+for mtr in "WOP" "CPT" "TTI" "TTO" "MNX"
+do
+    python FMS_clsCompileML.py $USR $DRV $AOI $mtr
+done
+python FMS_clsUnifyML.py $USR $DRV $AOI $THS 
+###############################################################################
+# Launch Scripts (SA)
+###############################################################################
+for mtr in "WOP" "CPT" "POE" "TTI" "TTO"
+do
+    python FMS_saAnalyzer.py $USR $DRV $QNT $AOI $THS $mtr
+done
 ###############################################################################
 # Train ML
 ###############################################################################
