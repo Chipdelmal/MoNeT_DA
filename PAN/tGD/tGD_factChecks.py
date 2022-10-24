a6a78802d88de2753b9513eefa0c94e33c25ab98
chipdelmal <chipdelmal@gmail.com>
2022-10-24 09:10:02 -0700

    tGD Fact checks done

--- PAN/tGD/tGD_factChecks.py
+++ PAN/tGD/tGD_factChecks.py
@@ -2,18 +2,20 @@
 # -*- coding: utf-8 -*-
 
 import sys
+import operator as op
 from os import path
 import numpy as np
 from glob import glob
 import tGD_aux as aux
+import matplotlib.pyplot as plt
 from datetime import datetime
 import compress_pickle as pkl
 from more_itertools import locate
 from joblib import Parallel, delayed
 import MoNeT_MGDrivE as monet
 
-(USR, DRV, AOI, QNT) = ('srv', 'linkedDrive', 'HLT', '50')
-probe = "E_499023_499023_007346_039812_941064_739794_01_0192"
+(USR, DRV, AOI, QNT) = ('srv2', 'splitDrive', 'TRS', '50')
+probe = "E_3500_3500_0000_0427_100_090_12_0100"
 
 exp = '100'
 ###############################################################################
@@ -28,9 +30,15 @@ monet.printExperimentHead(PT_PRE, PT_OUT, tS, 'tGD PstFraction '+AOI)
 # Get data
 ###############################################################################
 aType = ('HLT', 'WLD', 'TRS', 'CST')
+ix = aType.index(AOI)
 fNames = [glob(path.join(PT_OUT, probe+'-{}*'.format(i)))[0] for i in aType]
-dta = np.load(fNames[0])
+dta = np.load(fNames[ix])
 ###############################################################################
 # Analyze
 ###############################################################################
 avgSig = np.mean(dta, axis=0)
+minWild = np.min(avgSig)
+boolList = monet.comparePopToThresh(avgSig, [minWild, ], cmprOp=op.le)
+minTime = monet.trueIndices(boolList)[0]
+print(f'{minWild} ({1-minWild}) @ {minTime}')
+plt.plot(avgSig)
