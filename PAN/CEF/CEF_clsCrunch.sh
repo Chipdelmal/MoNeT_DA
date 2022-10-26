88d449b7ac4aaadc7d91c7e9bdc669e24894292f
chipdelmal <chipdelmal@gmail.com>
2022-10-26 16:56:56 -0700

    CEF Uncommenting parts of clsCrunch

--- PAN/CEF/CEF_clsCrunch.sh
+++ PAN/CEF/CEF_clsCrunch.sh
@@ -14,21 +14,21 @@ cd "$parent_path"
 ###############################################################################
 # Launch Scripts
 ###############################################################################
-# for mtr in "WOP" "CPT" "POE" "TTI" "TTO" "MNX"
-# do
-#     python CEF_clsCompile.py $USR $DRV $QNT $AOI $mtr
-# done
-# python CEF_clsUnify.py $USR $DRV $QNT $AOI $THS 
-# python CEF_dtaExplore.py $USR $DRV $QNT $AOI $THS $AOI
-# python CEF_dtaTraces.py $USR $DRV $QNT $AOI
-# ###############################################################################
-# # Launch Scripts (ML)
-# ###############################################################################
-# for mtr in "WOP" "CPT" "TTI" "TTO" "MNX"
-# do
-#     python CEF_clsCompileML.py $USR $DRV $AOI $mtr
-# done
-# python CEF_clsUnifyML.py $USR $DRV $AOI $THS 
+for mtr in "WOP" "CPT" "POE" "TTI" "TTO" "MNX"
+do
+    python CEF_clsCompile.py $USR $DRV $QNT $AOI $mtr
+done
+python CEF_clsUnify.py $USR $DRV $QNT $AOI $THS 
+python CEF_dtaExplore.py $USR $DRV $QNT $AOI $THS $AOI
+python CEF_dtaTraces.py $USR $DRV $QNT $AOI
+###############################################################################
+# Launch Scripts (ML)
+###############################################################################
+for mtr in "WOP" "CPT" "TTI" "TTO" "MNX"
+do
+    python CEF_clsCompileML.py $USR $DRV $AOI $mtr
+done
+python CEF_clsUnifyML.py $USR $DRV $AOI $THS 
 ###############################################################################
 # Launch Scripts (SA)
 ###############################################################################
@@ -39,10 +39,10 @@ done
 ###############################################################################
 # Train ML
 ###############################################################################
-# for mtr in "WOP" "CPT" "TTI"
-# do
-#     python CEF_mlrTrainML.py $USR $DRV $AOI $THS $mtr
-# done
+for mtr in "WOP" "CPT" "TTI"
+do
+    python CEF_mlrTrainML.py $USR $DRV $AOI $THS $mtr
+done
 ###############################################################################
 # Plot SA
 ###############################################################################
