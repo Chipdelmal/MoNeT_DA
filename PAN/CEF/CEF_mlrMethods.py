c9f2c4b5e9113f9945038cdf0a30063766a0bd5a
chipdelmal <chipdelmal@gmail.com>
2022-11-08 19:10:54 -0800

    CEF Compiling a cleaner trainer

---
+++ PAN/CEF/CEF_mlrMethods.py
@@ -0,0 +1,89 @@
+
+from xgboost import XGBRFRegressor
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.neural_network import MLPRegressor
+from sklearn.model_selection import GridSearchCV
+from sklearn.ensemble import RandomForestRegressor, AdaBoostRegressor
+from sklearn.ensemble import GradientBoostingRegressor, HistGradientBoostingRegressor
+import CEF_aux as aux
+
+
+def selectML(method, MOI):
+    if method=='rf':
+        modID = 'rdf'
+        rf = RandomForestRegressor(
+            n_estimators=100, max_depth=None, max_features="sqrt",
+            oob_score=True, criterion='squared_error',
+            n_jobs=aux.JOB_DSK*2, 
+            verbose=True
+        )
+    elif method=='ada':
+        rf = AdaBoostRegressor(
+            DecisionTreeRegressor(max_depth=50), 
+            loss='linear', n_estimators=100
+        )
+    elif method=='gb':
+        rf = GradientBoostingRegressor(
+            n_estimators=100, max_leaf_nodes=75, max_depth=30, 
+            verbose=True
+        )
+    elif method=='hgb':
+        modID = 'hgb'
+        if MOI=='CPT':
+            mono = aux.SA_MONOTONIC_CPT
+        else:
+            mono = aux.SA_MONOTONIC_WOP
+        rf = HistGradientBoostingRegressor(
+            loss='quantile', quantile=.25, 
+            max_iter=1000, max_leaf_nodes=75, max_depth=None, 
+            max_bins=255,
+            monotonic_cst=list(mono.values()),
+            verbose=True
+        )
+    if method=='rfg':
+        rf_o = RandomForestRegressor(
+            oob_score=True, criterion='squared_error',
+            n_jobs=aux.JOB_DSK*2, 
+            verbose=False
+        )
+        param_grid = [{
+            'n_estimators': [50, 100, 200], 
+            'max_depth':    [20, 25, 30, 50, None],
+            'max_features': ['sqrt', 'log2', None]
+        }]
+        rf = GridSearchCV(
+            estimator=rf_o, param_grid=param_grid,
+            n_jobs=8, verbose=2
+        )
+    elif method=='gbg':
+        rf_o = GradientBoostingRegressor()
+        param_grid = [{
+            'n_estimators':     [50, 100, 200], 
+            'max_leaf_nodes':   [20, 30, 50, 75, None],
+            'max_depth':        [20, 30, None]
+        }]
+        rf = GridSearchCV(
+            estimator=rf_o, param_grid=param_grid,
+            n_jobs=8, verbose=2
+        )
+    elif method=='xgb':
+        if MOI=='CPT':
+            mono = aux.SA_MONOTONIC_CPT
+        else:
+            mono = aux.SA_MONOTONIC_WOP
+        rf = XGBRFRegressor(
+            n_jobs=aux.JOB_DSK*2,
+            n_estimators=500, max_depth=None,
+            tree_method='hist',
+            max_bin=5000,
+            objective='reg:squarederror',
+            eval_metric='mae',
+            verbose=2
+        )
+    elif method=='mlp':
+        modID = 'mlp'
+        rf = MLPRegressor(
+            hidden_layer_sizes=(100, 50), 
+            verbose=True
+        )
+    return (rf, modID)
--- PAN/CEF/CEF_mlrTrainClean.py
+++ PAN/CEF/CEF_mlrTrainClean.py
@@ -1,31 +1,26 @@
 
 import sys
 import shap
-from os import path
 import numpy as np
+from os import path
 import pandas as pd
-import compress_pickle as pkl
-from datetime import datetime
 import rfpimp as rfp
 from math import ceil
+import compress_pickle as pkl
 import matplotlib.pyplot as plt
-from xgboost import XGBRFRegressor
+from datetime import datetime
+from sklearn.preprocessing import MinMaxScaler
 from sklearn.metrics import r2_score, explained_variance_score
 from sklearn.metrics import d2_absolute_error_score, median_absolute_error
 from sklearn.metrics import mean_absolute_error, mean_squared_error
 from sklearn.model_selection import train_test_split, cross_validate
-from sklearn.tree import DecisionTreeRegressor
-from sklearn.ensemble import RandomForestRegressor, AdaBoostRegressor
-from sklearn.ensemble import GradientBoostingRegressor, HistGradientBoostingRegressor
-from sklearn.model_selection import GridSearchCV, ShuffleSplit
+from sklearn.model_selection import ShuffleSplit
 from sklearn.inspection import permutation_importance
 from sklearn.inspection import PartialDependenceDisplay
 import MoNeT_MGDrivE as monet
 import CEF_aux as aux
 import CEF_gene as drv
-# https://shap.readthedocs.io/en/latest/index.html
-# https://mljar.com/blog/feature-importance-in-random-forest/
-# https://www.kaggle.com/code/vikumsw/explaining-random-forest-model-with-shapely-values
+import CEF_mlrMethods as mth
 
 if monet.isNotebook():
     (USR, DRV, AOI, THS, MOI) = ('srv', 'PGS', 'HLT', '0.1', 'CPT')
@@ -66,125 +61,47 @@ df = pd.read_csv(path.join(PT_OUT, fName))
 indVars = [i[0] for i in aux.DATA_HEAD]
 dfIn = df[indVars].drop('i_grp', axis=1)
 (X, y) = (dfIn, df[MOI])
-(X_train, X_test, y_train, y_test) = train_test_split(X, y, test_size=0.2)
-# scaler = StandardScaler().fit(X_train)
-# X_trainSC = scaler.transform(X_train)
+(X_trainR, X_testR, y_train, y_test) = train_test_split(X, y, test_size=0.2)
+# Scale input -----------------------------------------------------------------
+SCALER = MinMaxScaler(feature_range=(0, 1))
+SCALER.fit(X_trainR)
+(X_train, X_test) = (SCALER.transform(X_trainR), SCALER.transform(X_testR))
 ###############################################################################
 # Train Model
 ###############################################################################
-(MOD_SEL, K_SPLITS, T_SIZE) = ('rf', 10, .1)
+(MOD_SEL, K_SPLITS, T_SIZE) = ('mlp', 10, .1)
 scoring = [
     'explained_variance', 'max_error',
     'neg_mean_absolute_error', 'neg_root_mean_squared_error', 'r2'
 ]
-# Select the type of regressor ------------------------------------------------
-if MOD_SEL=='rf':
-    modID = ''
-    rf = RandomForestRegressor(
-        n_estimators=100, max_depth=None, max_features="sqrt",
-        oob_score=True, criterion='squared_error',
-        n_jobs=aux.JOB_DSK*2, 
-        verbose=False
-    )
-    param_grid = [{
-        'n_estimators': [50, 100], 
-        'max_depth':    ['20', '50', None]
-    }]
-    clf = GridSearchCV(
-        estimator=rf, param_grid=param_grid,
-        n_jobs=20
-    )
-elif MOD_SEL=='ada':
-    rf = AdaBoostRegressor(
-        DecisionTreeRegressor(max_depth=50), 
-        loss='linear', n_estimators=100
-    )
-elif MOD_SEL=='gb':
-    rf = GradientBoostingRegressor(
-        n_estimators=100, max_leaf_nodes=75, max_depth=30, 
-        verbose=True
-    )
-elif MOD_SEL=='hgb':
-    modID = 'hgb'
-    if MOI=='CPT':
-        mono = aux.SA_MONOTONIC_CPT
-    else:
-        mono = aux.SA_MONOTONIC_WOP
-    rf = HistGradientBoostingRegressor(
-        loss='quantile', quantile=.25, 
-        max_iter=1000, max_leaf_nodes=75, max_depth=None, 
-        max_bins=255,
-        monotonic_cst=list(mono.values()),
-        verbose=True
-    )
-if MOD_SEL=='rfg':
-    rf_o = RandomForestRegressor(
-        oob_score=True, criterion='squared_error',
-        n_jobs=aux.JOB_DSK*2, 
-        verbose=False
-    )
-    param_grid = [{
-        'n_estimators': [50, 100, 200], 
-        'max_depth':    [20, 25, 30, 50, None],
-        'max_features': ['sqrt', 'log2', None]
-    }]
-    rf = GridSearchCV(
-        estimator=rf_o, param_grid=param_grid,
-        n_jobs=8, verbose=2
-    )
-elif MOD_SEL=='gbg':
-    rf_o = GradientBoostingRegressor()
-    param_grid = [{
-        'n_estimators':     [50, 100, 200], 
-        'max_leaf_nodes':   [20, 30, 50, 75, None],
-        'max_depth':        [20, 30, None]
-    }]
-    rf = GridSearchCV(
-        estimator=rf_o, param_grid=param_grid,
-        n_jobs=8, verbose=2
-    )
-elif MOD_SEL=='xgb':
-    if MOI=='CPT':
-        mono = aux.SA_MONOTONIC_CPT
-    else:
-        mono = aux.SA_MONOTONIC_WOP
-    rf = XGBRFRegressor(
-        n_jobs=aux.JOB_DSK*2,
-        n_estimators=500, max_depth=None,
-        tree_method='hist',
-        max_bin=5000,
-        objective='reg:squarederror',
-        eval_metric='mae',
-        verbose=2
-    )
-elif MOD_SEL=='mlp':
-    modID = 'mlp-'
-    rf = MLPRegressor(
-        hidden_layer_sizes=(100, 50), 
-        verbose=True
-    )
-# Cross-validate --------------------------------------------------------------
-cv = ShuffleSplit(n_splits=K_SPLITS, test_size=T_SIZE)
-scores = cross_validate(
-    rf, X_train, y_train, cv=cv, scoring=scoring, n_jobs=K_SPLITS
-)
 ###############################################################################
 # Train Model
 ###############################################################################
+(rf, modID) = mth.selectML(MOD_SEL, MOI)
+fNameOut = '{}_{}T_{}-{}-MLR'.format(AOI, int(float(THS)*100), MOI, modID)
+# Train and Score -------------------------------------------------------------
 rf.fit(X_train, y_train)
 y_pred = rf.predict(X_test)
 scoresFinal = {
+    'r2': r2_score(y_test, y_pred),
     'root_mean_squared_error': mean_squared_error(y_test, y_pred, squared=False),
     'mean_absolute_error': mean_absolute_error(y_test, y_pred),
     'median_absolute_error ': median_absolute_error(y_test, y_pred),
     'explained_variance': explained_variance_score(y_test, y_pred),
-    'r2': r2_score(y_test, y_pred),
     'd2_absolute_error_score': d2_absolute_error_score(y_test, y_pred)
 }
 scoresFinal['r2Adj'] = aux.adjRSquared(
     scoresFinal['r2'], y_pred.shape[0], X_train.shape[1]
 )
 print(scoresFinal)
+# Cross-validate --------------------------------------------------------------
+cv = ShuffleSplit(n_splits=K_SPLITS, test_size=T_SIZE)
+scores = cross_validate(
+    rf, X_train, y_train, cv=cv, scoring=scoring, n_jobs=K_SPLITS
+)
+###############################################################################
+# Importance
+###############################################################################
 # Permutation scikit ----------------------------------------------------------
 perm_importance = permutation_importance(rf, X_test, y_test)
 sorted_idx = perm_importance.importances_mean.argsort()
@@ -208,7 +125,6 @@ shapVals = np.abs(shap_values).mean(0)
 # PDP/ICE Plots
 ###############################################################################
 clr = '#3a86ff' if MOI=='CPT' else '#03045e' 
-fNameOut = '{}_{}T_{}-{}MLR.png'.format(AOI, int(float(THS)*100), MOI, modID)
 (fig, ax) = plt.subplots(figsize=(16, 2))
 display = PartialDependenceDisplay.from_estimator(
     rf, X, indVars[:-1], ax=ax,
@@ -233,28 +149,27 @@ for r in range(len(display.axes_)):
         except:
             continue
 display.figure_.savefig(
-    path.join(PT_IMG, fNameOut), 
+    path.join(PT_IMG, fNameOut+'.png'), 
     facecolor='w', bbox_inches='tight', pad_inches=0.1, dpi=300
 )
 ###############################################################################
 # Interaction
 ###############################################################################
-display = PartialDependenceDisplay.from_estimator(
-    rf, X, features=[['i_pct', 'i_pmd']], 
-    subsample=2000, n_jobs=aux.JOB_DSK*4,
-    n_cols=ceil((len(indVars)-1)), 
-    kind='average', grid_resolution=200, random_state=0,
-    ice_lines_kw={'linewidth': 0.1, 'alpha': 0.1},
-    pd_line_kw={'color': '#f72585'}
-)
+# display = PartialDependenceDisplay.from_estimator(
+#     rf, X, features=[['i_pct', 'i_pmd']], 
+#     subsample=2000, n_jobs=aux.JOB_DSK*4,
+#     n_cols=ceil((len(indVars)-1)), 
+#     kind='average', grid_resolution=200, random_state=0,
+#     ice_lines_kw={'linewidth': 0.1, 'alpha': 0.1},
+#     pd_line_kw={'color': '#f72585'}
+# )
 ###############################################################################
 # Dump Model to Disk
 ###############################################################################
-fNameOut = '{}_{}T_{}-MLR.png'.format(AOI, int(float(THS)*100), MOI)
-fName = fNameOut[:-3]+'pkl'
-pkl.dump(rf, path.join(PT_OUT, fName))
-pd.DataFrame(scores).to_csv(path.join(PT_OUT, fName[:-4]+'_CV.csv'))
-pd.DataFrame(scoresFinal, index=[0]).to_csv(path.join(PT_OUT, fName[:-4]+'_VL.csv'))
+pkl.dump(rf, path.join(PT_OUT, fNameOut+'.pkl'))
+pkl.dump(SCALER, path.join(PT_OUT, fNameOut+'_NRM.pkl'))
+pd.DataFrame(scores).to_csv(path.join(PT_OUT, fNameOut+'_CV.csv'))
+pd.DataFrame(scoresFinal, index=[0]).to_csv(path.join(PT_OUT, fNameOut+'_VL.csv'))
 ###############################################################################
 # Dump Importances to Disk
 ###############################################################################
@@ -264,11 +179,8 @@ permSci = pd.DataFrame({
     'mean': perm_importance['importances_mean'], 
     'std': perm_importance['importances_std']
 })
-shapImp = pd.DataFrame({
-    'names': iVars,
-    'mean': shapVals
-})
+shapImp = pd.DataFrame({'names': iVars, 'mean': shapVals})
 permRF = impPM.reset_index()
-permSci.to_csv(path.join(PT_OUT, fNameOut[:-4]+'_PMI-SCI.csv'), index=False)
-permRF.to_csv(path.join(PT_OUT, fNameOut[:-4]+'_PMI-RFI.csv'), index=False)
-shapImp.to_csv(path.join(PT_OUT, fNameOut[:-4]+'_SHP-SHP.csv'), index=False)
+permSci.to_csv(path.join(PT_OUT, fNameOut+'_PMI-SCI.csv'), index=False)
+permRF.to_csv(path.join(PT_OUT,  fNameOut+'_PMI-RFI.csv'), index=False)
+shapImp.to_csv(path.join(PT_OUT, fNameOut+'_SHP-SHP.csv'), index=False)
