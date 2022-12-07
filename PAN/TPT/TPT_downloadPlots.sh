4c4c0f58ed906a95286b1c9b57ded279aadafac1
chipdelmal <chipdelmal@gmail.com>
2022-12-07 15:01:22 -0600

    TPT Fixing path erros for downloader

--- PAN/TPT/TPT_downloadPlots.sh
+++ PAN/TPT/TPT_downloadPlots.sh
@@ -6,7 +6,6 @@ SPE=$1 # TP13_figure_coluzii_low
 EXP=("X0001" "X2500" "X5000" "X7500" "X10000")
 for idx in "${!EXP[@]}"; do
     exp="${EXP[$idx]}"
-    scp -r \
-        "lab:lab:/RAID5/marshallShare/$SPE/$exp/img" \ 
-        "/home/chipdelmal/Documents/WorkSims/TP13/$SPE/TP13_figure_coluzii_low/$exp"
+    scp -r "lab:/RAID5/marshallShare/$SPE/$exp/img" "/home/chipdelmal/Documents/WorkSims/TP13/TP13_Trim_05/$SPE/$exp"
 done
+
