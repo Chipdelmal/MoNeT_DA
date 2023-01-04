69ce9409c5dc50e31b6952fa9728511737e9fd50
chipdelmal <chipdelmal@gmail.com>
2023-01-04 13:47:41 -0800

    PGS Interactive HTML figures updated

--- PAN/PGS/PGS_dtaHeatmapExplore.py
+++ PAN/PGS/PGS_dtaHeatmapExplore.py
@@ -85,7 +85,7 @@ poeDFRaw['POE_C'] = [math.floor(i*10) for i in poeDFRaw['POE']]
 ###############################################################################
 # Explore ranges
 ###############################################################################
-poeDF = poeDFRaw[poeDFRaw['POE']>=0.0]
+poeDF = poeDFRaw[poeDFRaw['POE']>=0.75]
 min(poeDF['i_ren'].unique())
 min(poeDF['i_res'].unique())
 sorted(poeDF['i_mfr'].unique())
@@ -98,95 +98,104 @@ sorted(poeDF['i_fvb'].unique())
 ###############################################################################
 # Matplotlib
 ###############################################################################
-COLORS = [
-    '#f7258570', '#b5179e65', '#7209b760', '#560bad55', '#480ca850',
-    '#3a0ca345', '#3f37c940', '#4361ee35', '#4895ef30', '#4cc9f025',
-    '#B4E9F120'
-][::-1]
-jitter = np.random.normal(0, 0.01, size=(1, poeDF.shape[0]))[0]
-plt.scatter(
-    poeDF['mfr+fvb']+jitter, poeDF['ren*res'],
-    s=0.05, color=[COLORS[i] for i in poeDF['POE_C']]
-)
-plt.savefig(
-    path.join(path.dirname(path.realpath(__file__)), 'scatter.png'), 
-    dpi=500, bbox_inches='tight', transparent=False, pad_inches=0
-)
+# COLORS = [
+#     '#f7258570', '#b5179e65', '#7209b760', '#560bad55', '#480ca850',
+#     '#3a0ca345', '#3f37c940', '#4361ee35', '#4895ef30', '#4cc9f025',
+#     '#B4E9F120'
+# ][::-1]
+# jitter = np.random.normal(0, 0.01, size=(1, poeDF.shape[0]))[0]
+# plt.scatter(
+#     poeDF['mfr+fvb']+jitter, poeDF['ren*res'],
+#     s=0.05, color=[COLORS[i] for i in poeDF['POE_C']]
+# )
+# plt.savefig(
+#     path.join(path.dirname(path.realpath(__file__)), 'scatter.png'), 
+#     dpi=500, bbox_inches='tight', transparent=False, pad_inches=0
+# )
 
-COLORS = [
-    '#f7258550', '#b5179e50', '#7209b750', '#560bad50', '#480ca850',
-    '#3a0ca350', '#3f37c950', '#4361ee50', '#4895ef50', '#4cc9f050',
-    '#ECF9FD50'
-][::-1]
-jitter = np.random.normal(0, 0.01, size=(1, poeDF.shape[0]))[0]
-fig = plt.figure()
-ax = fig.add_subplot(projection='3d')
-ax.scatter(
-    poeDF['i_fvb']+jitter, poeDF['i_mfr']+jitter, poeDF['ren*res'],
-    s=0.05, color=[COLORS[i] for i in poeDF['POE_C']]
-)
-ax.show()
+# COLORS = [
+#     '#f7258550', '#b5179e50', '#7209b750', '#560bad50', '#480ca850',
+#     '#3a0ca350', '#3f37c950', '#4361ee50', '#4895ef50', '#4cc9f050',
+#     '#ECF9FD50'
+# ][::-1]
+# jitter = np.random.normal(0, 0.01, size=(1, poeDF.shape[0]))[0]
+# fig = plt.figure()
+# ax = fig.add_subplot(projection='3d')
+# ax.scatter(
+#     poeDF['i_fvb']+jitter, poeDF['i_mfr']+jitter, poeDF['ren*res'],
+#     s=0.05, color=[COLORS[i] for i in poeDF['POE_C']]
+# )
+# ax.show()
 ###############################################################################
 # HTML
 ###############################################################################
-# REN -------------------------------------------------------------------------
 jitter = np.random.normal(0, 0.01, size=(1, poeDF.shape[0]))[0]
 poeDF['fvb'] = poeDF['i_fvb']+jitter
 jitter = np.random.normal(0, 0.01, size=(1, poeDF.shape[0]))[0]
 poeDF['mfr'] = poeDF['i_mfr']+jitter
+# REN -------------------------------------------------------------------------
 fig = go.Figure(data=go.Scatter3d(
-    x=poeDF['fvb'], 
-    y=poeDF['mfr'], 
-    z=poeDF['i_ren'],
+    x=poeDF['fvb'], y=poeDF['mfr'], z=poeDF['i_ren'],
+    customdata=np.dstack((poeDF['i_res']*10, poeDF['POE']))[0],
     mode='markers',
     marker=dict(
         size=2,
         color=poeDF['POE'],
         colorscale='Purples',
-        opacity=0.5
-    )
+        opacity=0.35
+    ),
+    hovertemplate="fvb:%{x:.3f}<br>mfr:%{y:.3f}<br>ren:%{z:.3f}<br>res:%{customdata[0]:.3f}<br>POE:%{customdata[1]:.3f}"
 ))
-fig.write_html(
-    path.join(
-        path.dirname(path.realpath(__file__)), 
-        'scatter-ren.html'
+fig.update_layout(
+    scene = dict(
+        xaxis_title='female viability (fvb)',
+        yaxis_title='male fertility (mfr)',
+        zaxis_title='number of releases (ren)',
     )
 )
+fig.write_html(path.join(PT_IMG, 'scatter-ren.html'))
 # RES -------------------------------------------------------------------------
 fig = go.Figure(data=go.Scatter3d(
-    x=poeDF['fvb'], 
-    y=poeDF['mfr'], 
-    z=poeDF['i_res'],
+    x=poeDF['fvb'], y=poeDF['mfr'], z=poeDF['i_res']*10,
+    customdata=np.dstack((poeDF['i_ren'], poeDF['POE']))[0],
     mode='markers',
     marker=dict(
         size=2,
         color=poeDF['POE'],
         colorscale='Purples',
-        opacity=0.5
-    )
+        opacity=0.35
+    ),
+    hovertemplate="fvb:%{x:.3f}<br>mfr:%{y:.3f}<br>res:%{z:.3f}<br>ren:%{customdata[0]:.3f}<br>POE:%{customdata[1]:.3f}"
 ))
-fig.write_html(
-    path.join(
-        path.dirname(path.realpath(__file__)), 
-        'scatter-res.html'
+fig.update_layout(
+    scene = dict(
+        xaxis_title='female viability (fvb)',
+        yaxis_title='male fertility (mfr)',
+        zaxis_title='size of releases [eggs/adult] (ren)',
     )
 )
+fig.write_html(path.join(PT_IMG, 'scatter-res.html'))
 # REN*RES ---------------------------------------------------------------------
 fig = go.Figure(data=go.Scatter3d(
-    x=poeDF['fvb'], 
-    y=poeDF['mfr'], 
-    z=poeDF['ren*res'],
+    x=poeDF['fvb'], y=poeDF['mfr'], z=poeDF['ren*res']*10,
+    customdata=np.dstack((poeDF['i_ren'], poeDF['i_res']*10, poeDF['POE']))[0],
     mode='markers',
     marker=dict(
         size=2,
         color=poeDF['POE'],
         colorscale='Purples',
-        opacity=0.5
-    )
+        opacity=0.35
+    ),
+    hovertemplate="fvb:%{x:.3f}<br>mfr:%{y:.3f}<br>tot:%{z:.3f}<br>ren:%{customdata[0]:.3f}<br>res:%{customdata[1]:.3f}<br>POE:%{customdata[2]:.3f}"
 ))
-fig.write_html(
-    path.join(
-        path.dirname(path.realpath(__file__)), 
-        'scatter-ren_res.html'
+fig.update_layout(
+    scene = dict(
+        xaxis = dict(nticks=5, range=[0, 0.5]),
+        yaxis = dict(nticks=5, range=[0, 0.5]),
+        zaxis = dict(nticks=5, range=[0, 10e3]),
+        xaxis_title='female viability (fvb)',
+        yaxis_title='male fertility (mfr)',
+        zaxis_title='total released mosquitoes [eggs/adult] (ren*res)',
     )
 )
+fig.write_html(path.join(PT_IMG, 'scatter-ren_res.html'))
