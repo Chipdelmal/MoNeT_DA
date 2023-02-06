15a69b77bb4f8352c3d472e88817de59cb7623a0
chipdelmal <chipdelmal@gmail.com>
2023-02-06 09:23:12 -0800

    TPT Added new sets to download sh

--- PAN/TPT/TPT_downloadData.sh
+++ PAN/TPT/TPT_downloadData.sh
@@ -3,7 +3,7 @@
 PTH_I='lab:/RAID5/marshallShare'
 PTH_O='/home/chipdelmal/Documents/WorkSims/TP13/2023_01'
 
-EXP=("gambiae_0_low" "gambiae_10_low" "gambiae_20_low" "coluzzii_0_low" "coluzzii_10_low" "coluzzii_20_low" "coluzzii_10_med")
+EXP=("coluzzii_0_med" "coluzzii_10_med" "coluzzii_20_med" "gambiae_0_low" "gambiae_10_low" "gambiae_20_low" "coluzzii_0_low" "coluzzii_10_low" "coluzzii_20_low")
 for idx in "${!EXP[@]}"; do
     exp="${EXP[$idx]}"
     printf "Downloading: \n\t${PTH_I}/TP13_figure_${exp} \n\t${PTH_O}\n"
--- PAN/TPT/TPT_downloadPlots.sh
+++ PAN/TPT/TPT_downloadPlots.sh
@@ -3,7 +3,7 @@
 # SPE=$1 # TP13_figure_coluzii_low
 
 EXP=("X0001" "X2500" "X5000" "X7500" "X10000")
-SPE=("coluzzii_0_low" "coluzzii_10_low" "coluzzii_20_low" "coluzzii_10_med" "gambiae_0_low" "gambiae_10_low" "gambiae_20_low")
+SPE=("gambiae_0_low" "gambiae_10_low" "gambiae_20_low" "coluzzii_0_low" "coluzzii_10_low" "coluzzii_20_low" "coluzzii_0_med" "coluzzii_10_med" "coluzzii_20_med")
 for idx in "${!EXP[@]}"; do
     spe="${SPE[$idx]}"
     for idx in "${!EXP[@]}"; do
