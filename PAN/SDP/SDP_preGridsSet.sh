37e7843aa1970cf726b04196ee61d9158a9af8b5
chipdelmal <chipdelmal@gmail.com>
2023-05-31 13:37:28 -0700

    SDP Updating preGridsSet

--- PAN/SDP/SDP_preGridsSet.sh
+++ PAN/SDP/SDP_preGridsSet.sh
@@ -4,14 +4,14 @@
 
 # argv1: USR
 
-python SDP_preGrids.py $1 SIT
-python SDP_preGrids.py $1 IIT
-python SDP_preGrids.py $1 FSR
+# python SDP_preGrids.py $1 SIT
+# python SDP_preGrids.py $1 IIT
+# python SDP_preGrids.py $1 FSR
 python SDP_preGrids.py $1 PGS
-python SDP_preGrids.py $1 AXS
-python SDP_preGrids.py $1 CRS
+# python SDP_preGrids.py $1 AXS
+# python SDP_preGrids.py $1 CRS
 python SDP_preGrids.py $1 CRX
 python SDP_preGrids.py $1 CRY
-python SDP_preGrids.py $1 SDR
+# python SDP_preGrids.py $1 SDR
 python SDP_preGrids.py $1 SDX
 python SDP_preGrids.py $1 SDY
