1c8944bca78b1d5378125aa637c65eed6083fadb
chipdelmal <chipdelmal@gmail.com>
2024-01-15 11:19:18 -0800

    TPP Exporting heatmaps to correct path

--- PAN/TPP/TPP_mlrHeatmaps.py
+++ PAN/TPP/TPP_mlrHeatmaps.py
@@ -52,7 +52,7 @@ TICKS_HIDE = True
 PT_OUT = path.join(PT_ROT, 'ML')
 PT_OUT_THS = path.join(PT_ROT, f'ML{int(float(THS)*100)}')
 PT_IMG = path.join(PT_OUT, 'img')
-PT_IMG_THS = path.join(PT_OUT_THS, 'img')
+PT_IMG_THS = path.join(PT_OUT_THS, 'img', 'heatmaps')
 [monet.makeFolder(i) for i in [PT_OUT, PT_IMG, PT_OUT_THS, PT_IMG_THS]]
 PT_SUMS = path.join(PT_ROT, 'SUMMARY')
 # Time and head ---------------------------------------------------------------
@@ -102,128 +102,131 @@ print(pred)
     np.arange(0, .30, .05)
 )
 (ix, jx) = (-3, 0)
-
-
-
-(shcRan, sbcRan, rgrRan, hdrRan, infRan) = (
-    np.array([SHC_RAN[ix]]),
-    np.arange(0.80, 1.00+delta, delta),
-    np.arange(0.00, 0.20+delta, delta),
-    np.array([HDR_PAR]),
-    np.array([INF_RAN[jx]])
-)
-combos = np.array(list(product(*[shcRan, sbcRan, hdrRan, rgrRan, infRan])))
-pred = rf.predict(combos)
-if MOI=='WOP':
-    pred = pred*aux.XRAN[1]/365
-###############################################################################
-# Generate response surface
-###############################################################################
-(x, y, z) = (list(combos.T[1]), list(combos.T[3]), pred)
-(xMin, yMin) = (
-    min([i for i in sorted(list(set(x))) if i>0]),
-    min([i for i in sorted(list(set(y))) if i>0])
-)
-rs = monet.calcResponseSurface(
-    x, y, z, 
-    scalers=(1, 1, 1), mthd='linear', 
-    xAxis=xSca, yAxis=ySca,
-    DXY=(ngdx, ngdy)
-)
-###############################################################################
-# Levels and Ranges
-###############################################################################
-# Get ranges ------------------------------------------------------------------
-(a, b) = ((min(x), max(x)), (min(y), max(y)))
-(ran, rsG, rsS) = (rs['ranges'], rs['grid'], rs['surface'])
-# Contour levels --------------------------------------------------------------
-if MOI == 'WOP':
-    (zmin, zmax) = (0, 5)
-    lvls = np.arange(zmin*1, zmax*1, (zmax-zmin)/2)
-    cntr = [YEAR_THS]
-elif MOI == 'CPT':
-    (zmin, zmax) = (0, 1)
-    lvls = np.arange(zmin*1, zmax*1, (zmax-zmin)/20)
-    cntr = [.9]
-elif MOI == 'POE':
-    (zmin, zmax) = (0, 1)
-    lvls = np.arange(zmin*1, zmax*1, (zmax-zmin)/10)
-    cntr = [.9]
-    lvls = [cntr[0]-.00001, cntr[0]]
-(scalers, HD_DEP, _, cmap) = aux.selectDepVars(MOI)
-cmap = monet.generateAlphaColorMapFromColor('#03045eAA')
-###############################################################################
-# Plot
-###############################################################################
-(fig, ax) = plt.subplots(figsize=(10, 10))
-xy = ax.plot(rsG[0], rsG[1], 'k.', ms=1, alpha=0.25, marker='1')
-cc = ax.contour(
-    rsS[0], rsS[1], rsS[2], 
-    levels=cntr, colors='#2b2d42',
-    linewidths=2, alpha=.9, linestyles='solid'
-)
-cs = ax.contourf(
-    rsS[0], rsS[1], rsS[2], 
-    linewidths=0,
-    levels=[0, YEAR_THS, 5], cmap=cmap, extend='max',
-    vmin=YEAR_THS-0.1
-)
-cs.cmap.set_under('#ffffff00')
-# Color bar ---------------------------------------------------------------
-if not TICKS_HIDE:
-    cbar = fig.colorbar(cs)
-    cbar.ax.get_yaxis().labelpad = 25
-    cbar.ax.set_ylabel('{}'.format(MOI), fontsize=15, rotation=270)
-# Grid and ticks ----------------------------------------------------------
-if xSca == 'log':
-    gZeroX = [i for i in list(sorted(set(x))) if i>0] 
-    ax.set_xticks([i/scalers[0] for i in gZeroX])
-    ax.axes.xaxis.set_ticklabels(gZeroX)
-else:
-    ax.set_xticks([i/scalers[0] for i in list(sorted(set(x)))])
-    ax.axes.xaxis.set_ticklabels(sorted(set(x)))
-if ySca == 'log':
-    gZeroY = [i for i in list(sorted(set(y))) if i>0] 
-    ax.set_yticks([i/scalers[1] for i in gZeroY])
-    ax.axes.yaxis.set_ticklabels(gZeroY)
-else:
-    ax.set_yticks([i/scalers[1] for i in list(sorted(set(y)))])
-    ax.axes.yaxis.set_ticklabels(sorted(set(y)))
-ax.grid(which='major', axis='x', lw=.1, alpha=0.3, color=(0, 0, 0))
-ax.grid(which='major', axis='y', lw=.1, alpha=0.3, color=(0, 0, 0))
-# Axes scales and limits --------------------------------------------------
-ax.set_xscale(xSca)
-ax.set_yscale(ySca)
-plt.xlim(ran[0][0], ran[0][1])
-plt.ylim(ran[1][0], ran[1][1])
-if TICKS_HIDE:
-    ax.axes.xaxis.set_ticklabels([])
-    ax.axes.yaxis.set_ticklabels([])
-    # ax.axes.xaxis.set_visible(False)
-    # ax.axes.yaxis.set_visible(False)
-    ax.xaxis.set_tick_params(width=0)
-    ax.yaxis.set_tick_params(width=0)
-    ax.tick_params(
-        left=False, labelleft=False, bottom=False, labelbottom=False
-    )
-    ax.set_axis_off()
-fig.tight_layout()
-ax.set_aspect(1.0/ax.get_data_ratio(), adjustable='box')
-ax.set_facecolor("#00000000")
-###############################################################################
-# Export File
-###############################################################################
-# Generate filename -----------------------------------------------------------
-(shcName, sbcName, hdrName, rgrName, infName) = (
-    str(int(shcRan[0]*aux.DATA_SCA['i_shc'])).zfill(aux.DATA_PAD['i_shc']),
-    str(int(sbcRan[0]*aux.DATA_SCA['i_sbc'])).zfill(aux.DATA_PAD['i_sbc']),
-    str(int(hdrRan[0]*aux.DATA_SCA['i_hdr'])).zfill(aux.DATA_PAD['i_hdr']),
-    str(int(rgrRan[0]*aux.DATA_SCA['i_rgr'])).zfill(aux.DATA_PAD['i_rgr']),
-    str(int(infRan[0]*aux.DATA_SCA['i_inf'])).zfill(aux.DATA_PAD['i_inf'])
-)
-fName = f'E_{shcName}_X_{hdrName}_X_{infName}'
-fName = fName+'-{}Q_{}T'.format(QNT, str(int(float(THS)*100)))
-fig.savefig(
-    path.join('./tmp/', f'{fName}.png'), 
-    dpi=500, bbox_inches='tight', transparent=True, pad_inches=0
-)
+for ix in range(len(SHC_RAN)):
+    for jx in range(len(INF_RAN)):
+        (shcRan, sbcRan, rgrRan, hdrRan, infRan) = (
+            np.array([SHC_RAN[ix]]),
+            np.arange(0.80, 1.00+delta, delta),
+            np.arange(0.00, 0.20+delta, delta),
+            np.array([HDR_PAR]),
+            np.array([INF_RAN[jx]])
+        )
+        combos = np.array(list(product(*[shcRan, sbcRan, hdrRan, rgrRan, infRan])))
+        pred = rf.predict(combos)
+        if MOI=='WOP':
+            pred = pred*aux.XRAN[1]/365
+        ###############################################################################
+        # Generate response surface
+        ###############################################################################
+        (x, y, z) = (list(combos.T[1]), list(combos.T[3]), pred)
+        (xMin, yMin) = (
+            min([i for i in sorted(list(set(x))) if i>0]),
+            min([i for i in sorted(list(set(y))) if i>0])
+        )
+        rs = monet.calcResponseSurface(
+            x, y, z, 
+            scalers=(1, 1, 1), mthd='linear', 
+            xAxis=xSca, yAxis=ySca,
+            DXY=(ngdx, ngdy)
+        )
+        ###############################################################################
+        # Levels and Ranges
+        ###############################################################################
+        # Get ranges ------------------------------------------------------------------
+        (a, b) = ((min(x), max(x)), (min(y), max(y)))
+        (ran, rsG, rsS) = (rs['ranges'], rs['grid'], rs['surface'])
+        # Contour levels --------------------------------------------------------------
+        if MOI == 'WOP':
+            (zmin, zmax) = (0, 5)
+            lvls = np.arange(zmin*1, zmax*1, (zmax-zmin)/2)
+            cntr = [YEAR_THS]
+        elif MOI == 'CPT':
+            (zmin, zmax) = (0, 1)
+            lvls = np.arange(zmin*1, zmax*1, (zmax-zmin)/20)
+            cntr = [.9]
+        elif MOI == 'POE':
+            (zmin, zmax) = (0, 1)
+            lvls = np.arange(zmin*1, zmax*1, (zmax-zmin)/10)
+            cntr = [.9]
+            lvls = [cntr[0]-.00001, cntr[0]]
+        (scalers, HD_DEP, _, cmap) = aux.selectDepVars(MOI)
+        cmap = monet.generateAlphaColorMapFromColor('#03045eAA')
+        ###############################################################################
+        # Plot
+        ###############################################################################
+        (fig, ax) = plt.subplots(figsize=(10, 10))
+        xy = ax.plot(rsG[0], rsG[1], 'k.', ms=1, alpha=0.25, marker='1')
+        cc = ax.contour(
+            rsS[0], rsS[1], rsS[2], 
+            levels=cntr, colors='#2b2d42',
+            linewidths=2, alpha=.9, linestyles='solid'
+        )
+        cs = ax.contourf(
+            rsS[0], rsS[1], rsS[2], 
+            linewidths=0,
+            levels=[0, YEAR_THS, 5], cmap=cmap, extend='max',
+            vmin=YEAR_THS-0.1
+        )
+        cs.cmap.set_under('#ffffff00')
+        # Color bar ---------------------------------------------------------------
+        if not TICKS_HIDE:
+            cbar = fig.colorbar(cs)
+            cbar.ax.get_yaxis().labelpad = 25
+            cbar.ax.set_ylabel('{}'.format(MOI), fontsize=15, rotation=270)
+        # Grid and ticks ----------------------------------------------------------
+        if xSca == 'log':
+            gZeroX = [i for i in list(sorted(set(x))) if i>0] 
+            ax.set_xticks([i/scalers[0] for i in gZeroX])
+            ax.axes.xaxis.set_ticklabels(gZeroX)
+        else:
+            ax.set_xticks([i/scalers[0] for i in list(sorted(set(x)))])
+            ax.axes.xaxis.set_ticklabels(sorted(set(x)))
+        if ySca == 'log':
+            gZeroY = [i for i in list(sorted(set(y))) if i>0] 
+            ax.set_yticks([i/scalers[1] for i in gZeroY])
+            ax.axes.yaxis.set_ticklabels(gZeroY)
+        else:
+            ax.set_yticks([i/scalers[1] for i in list(sorted(set(y)))])
+            ax.axes.yaxis.set_ticklabels(sorted(set(y)))
+        ax.grid(which='major', axis='x', lw=.1, alpha=0.3, color=(0, 0, 0))
+        ax.grid(which='major', axis='y', lw=.1, alpha=0.3, color=(0, 0, 0))
+        # Axes scales and limits --------------------------------------------------
+        ax.set_xscale(xSca)
+        ax.set_yscale(ySca)
+        plt.xlim(ran[0][0], ran[0][1])
+        plt.ylim(ran[1][0], ran[1][1])
+        if TICKS_HIDE:
+            ax.axes.xaxis.set_ticklabels([])
+            ax.axes.yaxis.set_ticklabels([])
+            # ax.axes.xaxis.set_visible(False)
+            # ax.axes.yaxis.set_visible(False)
+            ax.xaxis.set_tick_params(width=0)
+            ax.yaxis.set_tick_params(width=0)
+            ax.tick_params(
+                left=False, labelleft=False, bottom=False, labelbottom=False
+            )
+            ax.set_axis_off()
+        fig.tight_layout()
+        ax.set_aspect(1.0/ax.get_data_ratio(), adjustable='box')
+        ax.set_facecolor("#00000000")
+        ###############################################################################
+        # Export File
+        ###############################################################################
+        # Generate filename -----------------------------------------------------------
+        (shcName, sbcName, hdrName, rgrName, infName) = (
+            str(int(shcRan[0]*aux.DATA_SCA['i_shc'])).zfill(aux.DATA_PAD['i_shc']),
+            str(int(sbcRan[0]*aux.DATA_SCA['i_sbc'])).zfill(aux.DATA_PAD['i_sbc']),
+            str(int(hdrRan[0]*aux.DATA_SCA['i_hdr'])).zfill(aux.DATA_PAD['i_hdr']),
+            str(int(rgrRan[0]*aux.DATA_SCA['i_rgr'])).zfill(aux.DATA_PAD['i_rgr']),
+            str(int(infRan[0]*aux.DATA_SCA['i_inf'])).zfill(aux.DATA_PAD['i_inf'])
+        )
+        fName = f'E_{shcName}_X_{hdrName}_X_{infName}'
+        fName = fName+'-{}Q_{}T'.format(QNT, str(int(float(THS)*100)))
+        # fig.savefig(
+        #     path.join('./tmp/', f'{fName}.png'), 
+        #     dpi=500, bbox_inches='tight', transparent=True, pad_inches=0
+        # )
+        fig.savefig(
+            path.join(PT_IMG_THS, f'{fName}.png'), 
+            dpi=500, bbox_inches='tight', transparent=True, pad_inches=0
+        )
